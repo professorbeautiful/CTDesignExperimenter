32d59e7d6e79e508373a21e6c46003bf8ef03f3b
Roger Day <notsofeo@gmail.com>
2013-02-11 14:05:53 -0500

    Adding original shiny stuff.

---
+++ server.R
@@ -0,0 +1,27 @@
+# install.packages('shiny')
+# install.packages('RJSONIO')
+# library("shiny")
+
+
+shinyServer(function(input, output) {
+  
+  output$main_plot <- reactivePlot(function() {
+    
+    hist(faithful$eruptions,
+         probability = TRUE,
+         breaks = as.numeric(input$n_breaks),
+         xlab = "Duration (minutes)",
+         main = "Geyser eruption duration")
+    
+    if (input$individual_obs) {
+      rug(faithful$eruptions)
+    }
+    
+    if (input$density) {
+      dens <- density(faithful$eruptions,
+                      adjust = input$bw_adjust)
+      lines(dens, col = "blue")
+    }
+    
+  })
+})
