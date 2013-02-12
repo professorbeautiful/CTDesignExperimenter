61fa80334d66028f97c816025423301ae531bc4a
Roger Day <notsofeo@gmail.com>
2013-02-12 12:02:45 -0500

    I moved references to input$  into the reactive functions, figuring that they need to parse contents looking for "input$".  But still no output.

--- server.R
+++ server.R
@@ -5,12 +5,11 @@ library("shiny")
 shinyServer(function(input, output) {
   tabName = input$myTabSet
   classNames = cq(BaseCharModelSpecifier,PopModelSpecifier,OutcomeModelSpecifier,DesignSpecifier,EvalSpecifier)
-  output$tabName = reactiveText(function()tabName)
+  output$tabName = reactiveText(function()input$myTabSet)
   output$tabNameName = reactiveText(function()"tabName")
-  if(tabName %in% classNames) {
-    className = tabName
-    output$object_table <- reactiveTable(function() {
-      data.frame(subClassNames(className))
-    })
-  }
+  output$object_table <- reactiveTable(function() {
+    if(input$myTabSet %in% classNames) {
+      data.frame(subClassNames(input$myTabSet))
+    } else "No Table here"
+  })
 })
