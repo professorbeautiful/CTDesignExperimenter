baa549572beebfae06ff9f200f96d40d8ac9cd4e
Roger Day <notsofeo@gmail.com>
2013-02-12 11:39:42 -0500

    Basic TabPanel is working OK. Added more reactive elements. The error message is gone. But still no reactive elements are showing on the page.

--- server.R
+++ server.R
@@ -3,8 +3,14 @@
 library("shiny")
 
 shinyServer(function(input, output) {
-  output$main_table <- reactiveTable(function() {
-    data.frame(OutcomeModelSpecifier=subClassNames(
-      input$className))
-  })
+  tabName = input$myTabSet
+  classNames = cq(BaseCharModelSpecifier,PopModelSpecifier,OutcomeModelSpecifier,DesignSpecifier,EvalSpecifier)
+  output$tabName = reactiveText(function()tabName)
+  output$tabNameName = reactiveText(function()"tabName")
+  if(tabName %in% classNames) {
+    className = tabName
+    output$object_table <- reactiveTable(function() {
+      data.frame(subClassNames(className))
+    })
+  }
 })
--- ui.R
+++ ui.R
@@ -2,15 +2,50 @@
 
 require(RBioinf)
 
+classNames = c(base="BaseCharModelSpecifier",
+               pop="PopModelSpecifier",
+               outcome="OutcomeModelSpecifier",
+               design="DesignSpecifier",
+               eval="EvalSpecifier")
+
+makeSpecifierTab = function(specifierName) {
+  shortName = names(classNames)[match(specifierName, classNames)]
+  tabPanel(shortName, 
+           h3(specifierName %&% "/" %&% shortName),
+           br(),
+           textOutput(outputId="tabName"),
+           textOutput(outputId="tabNameName"),
+           h3("2nd line"),
+           br(),
+           tableOutput(outputId="object_table"),
+           br(),
+           h3("after the table")
+  )
+}
+
+
+makeExperimentTab = function(){
+  h1("experiment tab")
+}
+makeResultsTab = function(){
+  h1("results tab")
+}
 shinyUI(bootstrapPage(
-  selectInput(inputId = "className",
-              label = "class name",
-              choices = c("OutcomeModelSpecifier",
-                          "DesignSpecifier",
-                          "EvalSpecifier"),
-              selected = "EvalSpecifier"),
-  tableOutput(outputId = "main_table")
-  ))
-
-
-
+    textOutput(outputId="tabName" ),
+    br(),
+    verbatimTextOutput("TITLE"),
+    br(),
+    tabsetPanel(id="myTabSet",
+              tabPanel(names(classNames)[1], 
+                       makeSpecifierTab(classNames[1])), 
+              tabPanel(names(classNames)[2], 
+                       makeSpecifierTab(classNames[2])), 
+              tabPanel(names(classNames)[3], 
+                       makeSpecifierTab(classNames[3])), 
+              tabPanel(names(classNames)[4], 
+                       makeSpecifierTab(classNames[4])), 
+              tabPanel(names(classNames)[5], 
+                       makeSpecifierTab(classNames[5])), 
+              tabPanel("doExperiment", makeExperimentTab()),
+              tabPanel("Results", makeResultsTab())
+  )))
