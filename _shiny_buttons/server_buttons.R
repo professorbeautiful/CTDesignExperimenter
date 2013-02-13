eec0dc8e5ad625f96a83385290fd2de14da0557d
Roger Day <notsofeo@gmail.com>
2013-02-13 13:32:22 -0500

    Switched over to radio-button style. Better. Still having problems with the table. How to extract the string to use as arg to subClassNames?
    Good to use options(shiny.trace=TRUE);
    added to server.R.

--- _shiny_buttons/server_buttons.R
+++ _shiny_buttons/server_buttons.R
@@ -2,31 +2,15 @@
 # install.packages('RJSONIO')
 library("shiny")
 library(datasets)
-
+options(shiny.trace=TRUE)
 shinyServer(function(input, output) {
-  #   output$main_plot <- reactivePlot(function() {
-  #     tabName = tryCatch(input$myTabSet, 
-  #                                  error=function(e)"no tab")
-  #     if(is.null(input$myTabSet)) plot(1:3,1:3)
-  #     else hist(faithful$eruptions,
-  #          probability = TRUE,
-  #          xlab = "Duration (minutes)",
-  #          main = tabName)
-  #   })
-  output$tabName = reactiveText(function()input$myTabSet)
-  output$tabNameName = reactiveText(function()"tabName")
+  specName = 
+    reactiveText(function()input$specChoice)
+  print(specName)
+  output$specName = specName  
   classNames = cq(BaseCharModelSpecifier,PopModelSpecifier,OutcomeModelSpecifier,DesignSpecifier,EvalSpecifier)
-  output$object_table_BaseCharModelSpecifier <- 
+  output$object_table <- 
     reactiveTable(function() {
-      data.frame(subClassNames(BaseCharModelSpecifier))
+      data.frame(subClassNames(specName))
     })
-#       for(className in classNames) {
-#     output[["object_table_" %&% className]] <- 
-#       reactiveTable(function() {
-#           (className %&% "No Table here yet")
-#       #    if(length(input$myTabSet) > 0 && input$myTabSet %in% classNames) {
-#       #data.frame(subClassNames(className))
-#       #    } else matrix("No Table here")
-#     })
-#   }
 })
--- _shiny_buttons/ui_buttons.R
+++ _shiny_buttons/ui_buttons.R
@@ -2,57 +2,27 @@
 
 require(RBioinf)
 
-classNames = c(base="BaseCharModelSpecifier",
+specClassNames = c(base="BaseCharModelSpecifier",
                pop="PopModelSpecifier",
                outcome="OutcomeModelSpecifier",
                design="DesignSpecifier",
                eval="EvalSpecifier")
+shortName = function(specifierName)
+  names(classNames)[match(specifierName, classNames)]
 
-makeSpecifierTab = function(specifierName) {
-  shortName = names(classNames)[match(specifierName, classNames)]
-  tabPanel(shortName, 
-           h3(specifierName %&% "/" %&% shortName),
-           br(),
-#            textOutput(outputId="tabName"),
-#            textOutput(outputId="tabNameName"),
-           h3("2nd line"),
-           br(),
-           tableOutput(outputId="object_table_" %&% specifierName),
-           br(),
-           h3("after the table")
-  )
-}
-
-
-makeExperimentTab = function(){
-  h1("experiment tab")
-}
-makeResultsTab = function(){
-  h1("results tab")
-}
-myTabsetPanel=tabsetPanel(id="myTabSet",
-                          tabPanel(names(classNames)[1], 
-                                   makeSpecifierTab(classNames[1])), 
-                          tabPanel(names(classNames)[2], 
-                                   makeSpecifierTab(classNames[2])), 
-                          tabPanel(names(classNames)[3], 
-                                   makeSpecifierTab(classNames[3])), 
-                          tabPanel(names(classNames)[4], 
-                                   makeSpecifierTab(classNames[4])), 
-                          tabPanel(names(classNames)[5], 
-                                   makeSpecifierTab(classNames[5])), 
-                          tabPanel("doExperiment", makeExperimentTab()),
-                          tabPanel("Results", makeResultsTab())
-)
-
-shinyUI(bootstrapPage(
-    h3("TITLE"),## this works, but verbatimTextOutput does not.
-    br(),
-    (textOutput(outputId="tabName")),
-    br(),
-    tableOutput(outputId="object_table_" %&% classNames[1]),  
-    br(),
-    myTabsetPanel
-#   mainPanel=verbatimTextOutput("Verbatim")
-#     mainPanel=plotOutput(outputId = "main_plot", height = "300px")  
-  ))
+shinyUI(pageWithSidebar(
+    headerPanel=headerPanel(title="Clinical Trial Experiment Platform", windowTitle="Clinical Trial Experiment Platform"),
+    sidebarPanel=sidebarPanel(
+      h3("CT element spec types"),
+      br(),
+      radioButtons("specChoice", "choose spec type",
+                   specClassNames),
+      br(),
+      h3("Actions-TODO-View")
+    ),
+    mainPanel=mainPanel(
+      h3("main panel"),
+      textOutput(outputId="specName")
+#      tableOutput(outputId="object_table")
+    )
+) )
