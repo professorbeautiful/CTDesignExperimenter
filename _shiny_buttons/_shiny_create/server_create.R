e96abee5d716d9b36f159934178f408ee667908a
Roger Day <notsofeo@gmail.com>
2013-02-17 19:47:11 -0500

    This version has the 4 actions. The first 2 work as before-- tabels of classes and objects. The 3rd is for building a model. Not working yet. In the side panel, the error is: "argument is of length zero".  In the main panel, "trying to get slot 'className' from an object of a basic class  ("NULL")."

--- _shiny_buttons/_shiny_create/server_create.R
+++ _shiny_buttons/_shiny_create/server_create.R
@@ -4,14 +4,15 @@ library("shiny")
 # library(datasets)
 require(RBioinf)
 
-options(shiny.trace=FALSE)
-
+options(shiny.trace=TRUE)
 
 specClassNames = c(`patient attributes`="BaseCharModelSpecifier",
                    `population models`="PopModelSpecifier",
                    `outcome models`="OutcomeModelSpecifier",
                    designs="DesignSpecifier",
                    `evaluation criteria`="EvalSpecifier")
+buildingModelIndices = rep(NA, 4)
+names(buildingModelIndices) = names(specClassNames)[-1]
 
 shortName = function(specifierName)
   names(specClassNames)[match(specifierName, specClassNames)]
@@ -23,29 +24,41 @@ instanceNames = function(className) {
 shinyServer(function(input, output) {
   output$actionChoice = reactiveText(function()
     input$viewChoice)
-  specName = reactiveText(function()input$specChoice)
-  print(specName)
-  output$specName = specName  
+#  specName = reactiveText(function()input$specChoice)
   output$mainPanelHeader = reactiveText(
-    function() { if(input$viewChoice 
-                  %in% c("View spec classes", "View spec objects"))
-      input$viewChoice %&% "for  " %&% 
-                   shortName(input$specChoice)  %&%
-                   " (class="    %&%
-                   input$specChoice %&% ")"
-    else input$viewChoice %&% ": not yet implemented"
-  })
+    function() { 
+      if(input$viewChoice == "View spec classes")
+        return(input$viewChoice %&% " for  " %&% 
+                 shortName(input$specChoiceClasses)  %&% 
+                 " (class="    %&%
+                 input$specChoiceClasses %&% ")")
+      else if(input$viewChoice == "View spec objects")
+        return(input$viewChoice %&% " for  " %&% 
+                 shortName(input$specChoiceModels)  %&% 
+                 " (class="    %&%
+                 input$specChoiceModels %&% ")")
+      else if(input$viewChoice == "Define one clinical trial")
+        return(input$viewChoice %&% ": pick an object of type " %&% 
+                 shortName(input$specChoiceModels)  %&%
+                 " (class="    %&%
+                 input$specChoiceModels %&% ")" %&%
+                 " by typing object number in this box")
+      else input$viewChoice %&% ": not yet implemented"
+    })
   classNames = cq(BaseCharModelSpecifier,PopModelSpecifier,OutcomeModelSpecifier,DesignSpecifier,EvalSpecifier)
   output$classes_table <- 
     reactiveTable(function() {
-      cat("\n==specChoice Class==\n")
-      theClasses = data.frame(c(input$specChoice,
+      cat("\n==specChoiceClasses Class==\n")
+      theClasses = data.frame(c(input$specChoiceClasses,
                                 subClassNames(
-                                  input$specChoice)))
-      names(theClasses) = "subClasses of " %&% input$specChoice
-      rownames(theClasses) = 
-        "<input type=\"radio\" name=\"chooseOneClass\" 
-        value=" %&% 1:nrow(theClasses) %&% ">"
+                                  input$specChoiceClasses)))
+      names(theClasses) = "subClasses of " %&% input$specChoiceClasses
+      rownames(theClasses) = NULL
+#       radioButtons = sapply(1:nrow(theClasses),
+#                             function(rownum)
+#                               HTML("<input type=\"radio\" name=\"chooseOneClass\" 
+#         value=" %&% rownum %&% ">"))
+#       theClasses$O = radioButtons 
       theClasses$slotNames = 
         sapply(theClasses[[1]], 
                FUN=function(theClass){
@@ -62,35 +75,83 @@ shinyServer(function(input, output) {
         )
       cat("\n==theClasses==\n")
       print(str(theClasses))
-      cat("radio value for chooseOneClass = ", input$chooseOneClass,"\n")
+      #cat("radio value for chooseOneClass = ", input$chooseOneClass,"\n")
       theClasses
     })
-  output$objects_table <- 
+  output$classes_table_nrows <- 
     reactiveTable(function() {
-      theObjects = data.frame(instanceNames(
-                                  input$specChoice))
-      names(theObjects) = "instance"
-      theObjects$class = 
-        sapply(theObjects$instance,
-                                FUN=function(obName)
-                                  class(get(obName))
-                                )
-      theObjects$requirements = 
-        sapply(theObjects[[1]], 
-               FUN=function(theObject){
-                 req = try(getRequirements(get(theObject)))
-                 if(class(req) == "try-error") return ("")
-                 return(paste(req, collapse="\n"))}
-        )
-      theObjects$provisions = 
-        sapply(theObjects[[1]], 
-               FUN=function(theObject){
-                 prov = try(getProvisions(get(theObject)))
-                 if(class(prov) == "try-error") return ("")
-                 return(paste(prov, collapse="\n"))}
-        )
-      cat("\n==theObjects==\n")
-      print(str(theObjects))
-      theObjects
+      nrow(output$classes_table)
     })
+  createObjectsTable = function() {
+    theSpecChoice = switch(input$viewChoice,
+                           `View spec objects`=input$specChoiceModels,
+                           `Define one clinical trial`=input$specChoiceOneCT
+                           )    
+    theObjects = data.frame(instanceNames(theSpecChoice))
+    names(theObjects) = "instance"
+    theObjects$class = 
+      sapply(theObjects$instance,
+             FUN=function(obName) class(get(obName)))
+    theObjects$requirements = 
+      sapply(theObjects[[1]], 
+             FUN=function(theObject){
+               req = try(getRequirements(get(theObject)))
+               if(class(req) == "try-error") return ("")
+               return(paste(req, collapse="\n"))}
+      )
+    theObjects$provisions = 
+      sapply(theObjects[[1]], 
+             FUN=function(theObject){
+               prov = try(getProvisions(get(theObject)))
+               if(class(prov) == "try-error") return ("")
+               return(paste(prov, collapse="\n"))}
+      )
+    cat("\n==theObjects==\n")
+    print(str(theObjects))
+    theObjects
+  }
+  output$objects_table <- reactiveTable()
+  output$objects_table_1 <- reactiveTable(createObjectsTable)
+  output$objects_table_2 <- reactiveTable(createObjectsTable)
+
+  output$buildingModelIndices = reactiveUI(function() {
+    cat("buildingModelIndices=", buildingModelIndices)
+    if(!is.null(input$specChoiceModels) & !is.null(input$model_row_num))
+      buildingModelIndices[isolate(print(input$specChoiceModels))] <<- input$model_row_num
+    ## The purpose of "isolate" here is to prevent 
+    buildingModelIndices
+  })
+  
+  f.buildingModelMain = function() {
+    tableOutput(outputId="objects_table_2")
+  }
+  output$buildingModelMain = reactiveUI(f.buildingModelMain)
+  
+  f.buildingModelSide = function() {
+    radioButtonLabels = paste(
+      specClassNames %except% "BaseCharModelSpecifier",
+      "[", input$buildingModelIndices, "]")
+    nrow_table = nrow(output$objects_table_2)
+    if(is.na(nrow_table)) nrow_table=10
+    return(list(
+      radioButtons("specChoiceOneCT", "One CT component type:", 
+                   radioButtonLabels),
+      #      textOutput("CurrentCTasText"),
+      numericInput("model_row_num", "model row num",
+                   "1", min=1, max=nrow_table)
+    ))
+  }
+  debug(f.buildingModelSide)
+  output$buildingModelSide = reactiveUI(f.buildingModelSide)
+  
+  #   output$objects_table_nrows <- 
+  #     reactiveTable(function() {
+  #       nrow(output$objects_table)
+  #     })
+  #   output$CurrentCT <- 
+  #     reactiveTable(function() {
+  #       specChoiceObjects      model_row_num
+  #       nrow(output$objects_table)
+  #     })
+  #   
 })
--- _shiny_buttons/ui.R
+++ _shiny_buttons/ui.R
@@ -14,12 +14,12 @@ addAttr = function(x, att, value) {attr(x, att) = value; x }
 asHTML = function(x) addAttr(x, "html", TRUE)
 
 shinyUI(pageWithSidebar(  
-#<style font='red' bg='yellow' > </style>
+  #<style font='red' bg='yellow' > </style>
   headerPanel=
-    addAttr("html", TRUE,
-x="<head>\n  <title>Clinical Trial Experiment Platform</title>\n</head>\n<div class=\"span12\" style=\"padding: 10px 0px;\">
-  <h2 color=rgb(200,300,400) > <i>Clinical Trial Experiment Platform </i> </h2>\n</div>") 
-#    headerPanel(title="Clinical Trial Experiment Platform", windowTitle="Clinical Trial Experiment Platform")
+    #    addAttr("html", TRUE,
+    #HTML("<head>\n  <title>Clinical Trial Experiment Platform</title>\n</head>\n<div class=\"span12\" style=\"padding: 10px 0px;\">)
+    #<h2 color=rgb(200,300,400) > <i>Clinical Trial Experiment Platform </i> </h2>\n</div>") 
+    headerPanel(title="Clinical Trial Experiment Platform", windowTitle="Clinical Trial Experiment Platform")
   ,
   sidebarPanel=sidebarPanel(
     #tag('font', varArgs=c(size=-2))
@@ -29,16 +29,29 @@ x="<head>\n  <title>Clinical Trial Experiment Platform</title>\n</head>\n<div cl
                     "View spec objects",
                     "Define one clinical trial",
                     "Do a CT experiment"))
-     , tag('hr', NULL)
-     , conditionalPanel(condition=
-                     "input.viewChoice==\"View spec classes\"
-                  ||  input.viewChoice==\"View spec objects\"",
-          radioButtons("specChoice", "choose spec type", specClassNames))
-#      , conditionalPanel(condition=
-#                     "input.viewChoice==\"Do a CT experiment\"
-#                   ||  input.viewChoice==\"Define one clinical trial\"",
-# #        textOutput("viewChoice")
-#        , "This view choice is not yet implemented." )
+    , tag('hr', NULL)
+    , conditionalPanel(condition=
+                         "input.viewChoice==\"View spec classes\"",
+                       "Choose spec class to view itsvsubclasses.",
+                       radioButtons("specChoiceClasses", "choose spec type", specClassNames)
+    )
+    , conditionalPanel(condition=
+                         "input.viewChoice==\"View spec objects\"",
+                       "Choose spec class to view its objects.",
+                       radioButtons("specChoiceModels", "choose spec type", 
+                                    specClassNames %except% "BaseCharModelSpecifier")
+    )
+    , conditionalPanel(condition=
+                         "input.viewChoice==\"Define one clinical trial\""
+                       #, "This view choice is not yet implemented." 
+                       ,    HTML("Select model object (by number) <br>to build simulation.")
+                       ,                   uiOutput("buildingModelSide")
+    )
+    , conditionalPanel(condition=
+                         "input.viewChoice==\"Do a CT experiment\""
+                       # #        textOutput("viewChoice")
+                       , "This view choice is not yet implemented." 
+    )
   ),
   mainPanel=mainPanel(
     # condition MUST BE VALID JS. 
@@ -49,15 +62,24 @@ x="<head>\n  <title>Clinical Trial Experiment Platform</title>\n</head>\n<div cl
     #   Error in tag("div", list(...)) : argument is missing, with no default
     # it's because of an extra comma--  empty arg.
     
-    h2(textOutput(outputId="mainPanelHeader"))
-    , conditionalPanel(condition=
-                           "input.viewChoice==\"View spec classes\"",
-                           tableOutput(outputId="classes_table")
-        )
-    ,  conditionalPanel(condition=
-                           "input.viewChoice==\"View spec objects\"",
-                          tableOutput(outputId="objects_table")
-        )    
-  )
-)
-)
+    h3(textOutput(outputId="mainPanelHeader"))
+    , conditionalPanel(
+      condition="input.viewChoice==\"View spec classes\"",
+      tableOutput(outputId="classes_table")
+    )
+    , conditionalPanel(
+      condition="input.viewChoice==\"View spec objects\"",
+      tableOutput(outputId="objects_table_1")
+    )
+    ,  conditionalPanel(
+      condition=
+        "input.viewChoice==\"Define one clinical trial\"",
+      "Spec selections for one CT:"
+      , uiOutput("buildingModelMain")
+    )
+    ,  conditionalPanel(
+      condition=
+        "input.viewChoice==\"Do a CT experiment\"",
+      ("NOT YET IMPLEMENTED")
+    )
+  )))
