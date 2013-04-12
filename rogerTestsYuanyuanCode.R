935c8ef6580c3412ac8d0caf4a69557d50878679
Roger Day <notsofeo@gmail.com>
2013-04-11 22:03:15 -0400

    Cosmetic changes only.  (I think!)

--- rogerTestsYuanyuanCode.R
+++ rogerTestsYuanyuanCode.R
@@ -1,82 +1,89 @@
+if(getNamespaceVersion(ns="CTDesignExplorer") != "1.0")
+  warning("version of CTDesignExplorer should be 1.0;  it is ", 
+          getNamespaceVersion(ns="CTDesignExplorer"))    
 library("CTDesignExplorer")
-crm9 <- new("CRMSpecifier",
-	InitialProbGuesses=c(0.1,0.2,0.3,0.4,0.5),
-	TargetProb=0.3,
-	SampleSize=21,
-	NPatsPerCohort=3,
-	StartingDoseLevel=3,
-	TierDoses=1:5,
-	InitialStageDoseLevels=NULL)
 
-designSpecs <- list(crm9)
-
-# PopModelSpecs = list(NULL)  ## incorrect for signature.
-
-setClass(Class="PKclearanceModel",
-         contains="BaseCharModelSpecifier",
-         representation=representation(
-           location="numeric", sd="numeric"),
-         prototype=prototype(
-           location=15, sd=0.5,
-           RGenFun="exp(rnorm(1,mean=log(baseCharModelSpec@location),
+# createCTDesignCatalog = function() {
+  crm9 <- new("CRMSpecifier",
+              InitialProbGuesses=c(0.1,0.2,0.3,0.4,0.5),
+              TargetProb=0.3,
+              SampleSize=21,
+              NPatsPerCohort=3,
+              StartingDoseLevel=3,
+              TierDoses=1:5,
+              InitialStageDoseLevels=NULL)
+  
+  designSpecs <- list(crm9)
+  
+  # PopModelSpecs = list(NULL)  ## incorrect for signature.
+  
+  setClass(Class="PKclearanceModel",
+           contains="BaseCharModelSpecifier",
+           representation=representation(
+             location="numeric", sd="numeric"),
+           prototype=prototype(
+             location=15, sd=0.5,
+             RGenFun="exp(rnorm(1,mean=log(baseCharModelSpec@location),
            sd=baseCharModelSpec@sd))")
-        )
-standardPKclearanceModel = new("PKclearanceModel",
-    BaseCharName = "PKclearance") 
-
-setClass(Class="ToxDoseThresholdModel",
-         contains="BaseCharModelSpecifier",
-         representation=representation(
-           location="numeric", sd="numeric"),
-         prototype=prototype(
-           ConditionBaseCharNames = "PKclearance",
-           location=1, sd=0.02,
-           RGenFun="PKclearance * exp(rnorm(baseCharModelSpec@location, sd=baseCharModelSpec@sd))")
-)
-
-setClass(Class="ToxDoseThresholdModel",
-         contains="BaseCharModelSpecifier",
-         representation=representation(
-           location="numeric", sd="numeric"),
-         prototype=prototype(
-           ConditionBaseCharNames = "PKclearance",
-           location=1, sd=0.02,
-           RGenFun="PKclearance * exp(rnorm(baseCharModelSpec@location, sd=baseCharModelSpec@sd))")
-)
-standardToxDoseThresholdModel = new("ToxDoseThresholdModel",
-            BaseCharName = "ToxDoseThreshold") 
-
-
-setClass(Class="EfficacyDoseThresholdModel",
-         contains="BaseCharModelSpecifier",
-         representation=representation(
-           location="numeric", sd="numeric"),
-         prototype=prototype(
-           ConditionBaseCharNames = "PKclearance",
-           location=1, sd=0.02,
-           RGenFun="PKclearance * exp(rnorm(baseCharModelSpec@location, sd=baseCharModelSpec@sd))")
-)
-standardEfficacyDoseThresholdModel = new("EfficacyDoseThresholdModel",
-  BaseCharName = "efficacyDoseThreshold") 
-
-doseThresholdPopModelSpec = new("PopModelSpecifier")  ## which is a list.
-doseThresholdPopModelSpec@PopModelSpec[[1]] = standardPKclearanceModel
-doseThresholdPopModelSpec@PopModelSpec[[2]] = standardToxDoseThresholdModel
-doseThresholdPopModelSpec@PopModelSpec[[3]] = standardEfficacyDoseThresholdModel
-# lapply(1:3, function(x)doseThresholdPopModelSpec
-#                   @PopModelSpec[[x]]@location)
-
-toxDoseThresholdOutcomeModel <- new("DoseThresholdModelSpecifier",
-                                 DoseThresholdName="ToxDoseThreshold")
-efficacyDoseThresholdOutcomeModel <- new("DoseThresholdModelSpecifier",
-                                 DoseThresholdName="EfficacyDoseThreshold")
-### currently the validation allows only these two DoseThresholdName.
+  )
+  standardPKclearanceModel = new("PKclearanceModel",
+                                 BaseCharName = "PKclearance") 
+  
+  setClass(Class="ToxDoseThresholdModel",
+           contains="BaseCharModelSpecifier",
+           representation=representation(
+             location="numeric", sd="numeric"),
+           prototype=prototype(
+             ConditionBaseCharNames = "PKclearance",
+             location=1, sd=0.02,
+             RGenFun="PKclearance * exp(rnorm(baseCharModelSpec@location, sd=baseCharModelSpec@sd))")
+  )
+  
+  setClass(Class="ToxDoseThresholdModel",
+           contains="BaseCharModelSpecifier",
+           representation=representation(
+             location="numeric", sd="numeric"),
+           prototype=prototype(
+             ConditionBaseCharNames = "PKclearance",
+             location=1, sd=0.02,
+             RGenFun="PKclearance * exp(rnorm(baseCharModelSpec@location, sd=baseCharModelSpec@sd))")
+  )
+  standardToxDoseThresholdModel = new("ToxDoseThresholdModel",
+                                      BaseCharName = "ToxDoseThreshold") 
+  
+  
+  setClass(Class="EfficacyDoseThresholdModel",
+           contains="BaseCharModelSpecifier",
+           representation=representation(
+             location="numeric", sd="numeric"),
+           prototype=prototype(
+             ConditionBaseCharNames = "PKclearance",
+             location=1, sd=0.02,
+             RGenFun="PKclearance * exp(rnorm(baseCharModelSpec@location, sd=baseCharModelSpec@sd))")
+  )
+  standardEfficacyDoseThresholdModel = new("EfficacyDoseThresholdModel",
+                                           BaseCharName = "efficacyDoseThreshold") 
+  
+  doseThresholdPopModelSpec = new("PopModelSpecifier")  ## which is a list.
+  doseThresholdPopModelSpec@PopModelSpec[[1]] = standardPKclearanceModel
+  doseThresholdPopModelSpec@PopModelSpec[[2]] = standardToxDoseThresholdModel
+  doseThresholdPopModelSpec@PopModelSpec[[3]] = standardEfficacyDoseThresholdModel
+  # lapply(1:3, function(x)doseThresholdPopModelSpec
+  #                   @PopModelSpec[[x]]@location)
+  
+  toxDoseThresholdOutcomeModel <- new("DoseThresholdModelSpecifier",
+                                      DoseThresholdName="ToxDoseThreshold")
+  efficacyDoseThresholdOutcomeModel <- new("DoseThresholdModelSpecifier",
+                                           DoseThresholdName="EfficacyDoseThreshold")
+  ### currently the validation allows only these two DoseThresholdName.
+  
+}
 
 showMethods("sim1CT", includeDefs=T)
 oneCTresult = sim1CT(designSpec=crm9,
-		popModelSpec=doseThresholdPopModelSpec,   ## correct.
-    #### only one outcome allowed in sim1CT call.
-    outcomeModelSpec=toxDoseThresholdOutcomeModel)
+                     popModelSpec=doseThresholdPopModelSpec,   ## correct.
+                     #### only one outcome allowed in sim1CT call.
+                     outcomeModelSpec=toxDoseThresholdOutcomeModel)
 oneCTresult
 
 
@@ -103,10 +110,10 @@ outcomeModelSpecs <- list(toxDoseThresholdOutcomeModel,
 anExperiment = doExperiment(
   designSpec=designSpecs,
   popModelSpecs=list(doseThresholdPopModelSpec), 
-	outcomeModelSpecs=list(toxDoseThresholdOutcomeModel, efficacyDoseThresholdOutcomeModel), 
-	evalSpecs=evalSpecs, 
-	nReps=2, 
-	seed=NULL, simDataDir="./", userInput=FALSE) 
+  outcomeModelSpecs=list(toxDoseThresholdOutcomeModel, efficacyDoseThresholdOutcomeModel), 
+  evalSpecs=evalSpecs, 
+  nReps=2, 
+  seed=NULL, simDataDir="./", userInput=FALSE) 
 length(anExperiment)  #  1;  just one scenario.
 length(anExperiment$Scenario1)  #  3 = length(EvalSpecs)
 anExperiment$Scenario1[[1]] #sample sizes, per rep;  summary => table
