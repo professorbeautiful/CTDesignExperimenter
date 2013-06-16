82934e920461b3e6ee3f334dc07f0115cd609215
Roger Day <notsofeo@gmail.com>
2013-06-16 10:01:14 -0400

    Bumped version, to make sure it was working. After doing     detach("package:CTDesignExplorer", unload=T)     library("CTDesignExplorer") the .onAttach greeting message appeared. Wrote a message "Run createCTDEcatalog" as a reminder... can't write into locked env.

--- CTDesignExplorer/R/zzz.R
+++ CTDesignExplorer/R/zzz.R
@@ -2,5 +2,7 @@
 .onAttach = function(libname, pkgname) {
 	desc <- packageDescription(pkgname)
 	packageStartupMessage("This is ", pkgname, " ", desc$Version, " ", desc$Date)
+	#	createCTDEcatalog()    # Can't write into locked env.
+  cat("Run createCTDEcatalog() to get started.\n")
 	return(invisible(NULL))
 }
