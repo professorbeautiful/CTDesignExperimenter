b37cad06d7ddbf646361867135fa455792cc28fb
Roger Day <notsofeo@gmail.com>
2013-06-16 14:01:01 -0400

    Added a "clear" convenience function. Based on mvbutils:::yes.no , but uses upper-casing based on the gsub help file solution.

--- CTDesignExplorer/R/createCTDEcatalog.R
+++ CTDesignExplorer/R/createCTDEcatalog.R
@@ -1,3 +1,19 @@
+clear = function(){
+  answer <- repeat {
+    cat("Delete ALL files in .GlobalEnv?\n  (cannot be undone): ")
+    answer <- readline()
+    answer <- gsub("(\\w)", "\\U\\1", answer, perl=T)
+    answer <- pmatch(answer, c("YES",  "NO", "N"))
+      if (!is.na(answer)) {
+        if(answer %in% 1)  
+        rm(list=ls(all=T, pos=1), pos=1)
+      else
+        cat("Aborted. No objects deleted.\n")
+      return(invisible(NULL))
+    }
+  }
+}
+
 createCTDEcatalog = function() {
   ### Based on rogerTestsYuanyuanCode.R
   #print(ls())  ### Initially, nothing.
