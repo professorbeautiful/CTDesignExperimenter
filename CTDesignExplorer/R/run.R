2bc79831f4d23c2635a45c48487bd448094d1bd9
Roger Day <notsofeo@gmail.com>
2013-06-16 14:28:13 -0400

    OK, I've got "run()" to work from the package! and, we do generate and view patients!

--- CTDesignExplorer/R/run.R
+++ CTDesignExplorer/R/run.R
@@ -1,8 +1,11 @@
 run = function() {
+  createCTDEcatalog()
   result = tryCatch( error = function(e) e,
     expr=require("shiny"))
   if(class(result) != "try-error")
     runApp(system.file(
-      package="CTDesignExplorer", "inst", "shinydocs"))
+      package="CTDesignExplorer", "shinydocs"))
+  ### NOTE that the folder inst/shinydocs 
+  ### becomes just shinydocs after building.
   return(invisible(NULL))
 }
