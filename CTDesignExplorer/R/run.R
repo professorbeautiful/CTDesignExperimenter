42be9c47a0c5300f24af72f183cbb434492ec164
Roger Day <notsofeo@gmail.com>
2013-06-18 06:06:25 -0400

    New options to run():   createCTDEcatalog=TRUE   background=FALSE
    The background option is commented out... but the RJSONIO warning message is probably irrelevant.

--- CTDesignExplorer/R/run.R
+++ CTDesignExplorer/R/run.R
@@ -1,5 +1,10 @@
-run = function() {
-  createCTDEcatalog()
+run = function(background=FALSE, createCTDEcatalog=TRUE) {
+  if(createCTDEcatalog) createCTDEcatalog()
+#   if(background) { ## Run it in the background outside of this session.
+#     system("echo 'library(CTDesignExplorer); run()' | R --vanilla")
+  #   PROBLEMS!  Warning in .simpleDuplicateClass(def, prev) :
+#  the specification for S3 class “AsIs” in package ‘BiocGenerics’ seems equivalent to one from package ‘RJSONIO’ and is not turning on duplicate class definitions for this class
+#   }
   result = tryCatch( error = function(e) e,
     expr=require("shiny"))
   if(class(result) != "try-error")
