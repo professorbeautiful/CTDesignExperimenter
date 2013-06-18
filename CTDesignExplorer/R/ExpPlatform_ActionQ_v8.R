5722120bf7d35c330af1b6373e10b60f597c19e7
Roger Day <notsofeo@gmail.com>
2013-06-18 14:50:21 -0400

    Added parameters, requirements and provisions as Specifier slots.
    Defined initialize method for
    BaseCharModelSpecifier
    Seems to work OK.
    
    Also some re-indenting.

--- CTDesignExplorer/R/ExpPlatform_ActionQ_v8.R
+++ CTDesignExplorer/R/ExpPlatform_ActionQ_v8.R
@@ -1,6 +1,13 @@
 rm(list=ls())
 
-setClass("Specifier")
+setClass("Specifier", 
+         representation=representation(
+           parameters="list", requirements="list",
+           provisions="list"),
+         prototype=list(parameters=list(), 
+                        requirements=list(),
+                        provisions=list())
+)
 
 ### Some utility classes and methods
 ## Class Union: OptionalCharacter
@@ -16,39 +23,42 @@ setClassUnion("NumericLogical",c("numeric","logical"))
 # This class represents data from a set of concurrent treatments for a patient
 # The number of concurrent treatments is an integer >= 1
 # TrtAllos is a data frame which may have columns for TrtName, Dose,Dose level, Unit, Route, StartTime and EndTime 
-setClass("ConcurrentTrtsData",representation(TrtAllos="data.frame",Outcomes="numeric",TimesToOutcomes="OptionalNumeric"),
-    prototype=list(TimesToOutcomes=NULL))
+setClass("ConcurrentTrtsData",
+         representation(TrtAllos="data.frame",Outcomes="numeric",TimesToOutcomes="OptionalNumeric"),
+         prototype=list(TimesToOutcomes=NULL))
 
 ## Class: PatData
 # This class represents data from a patient
 # ConcurrentTrtsDataList is a list of data from different sets of concurrent treatments
 # PatTimes may include enrollment time, off-CT time for a patient
-setClass("PatData",representation(ID="OptionalNumeric",BaseChars="OptionalNumeric",ConcurrentTrtsDataList="list",
-    PatTimes="OptionalNumeric"),
-    prototype=list(ID=NULL,BaseChars=NULL,PatTimes=NULL),
-    validity=function(object){
-        if (length(object@ConcurrentTrtsDataList)==0) TRUE
-        else{
-            if(! all(Check<-sapply(object@ConcurrentTrtsDataList, function(x) is(x,"ConcurrentTrtsData"))))
-                stop("The following elements are not objects of the ConcurrentTrtsData class:","\n",paste(which(!Check), collapse=","),"\n" )
-            else TRUE
-        }
-    }
+setClass("PatData",
+         representation(ID="OptionalNumeric",BaseChars="OptionalNumeric",ConcurrentTrtsDataList="list",
+                        PatTimes="OptionalNumeric"),
+         prototype=list(ID=NULL,BaseChars=NULL,PatTimes=NULL),
+         validity=function(object){
+           if (length(object@ConcurrentTrtsDataList)==0) TRUE
+           else{
+             if(! all(Check<-sapply(object@ConcurrentTrtsDataList, function(x) is(x,"ConcurrentTrtsData"))))
+               stop("The following elements are not objects of the ConcurrentTrtsData class:","\n",paste(which(!Check), collapse=","),"\n" )
+             else TRUE
+           }
+         }
 )
     
 ## Class: CTData
-# This class represents data from a single CT
+# This class represents data from a single CT (clinical trial).
 # CTTimes may include switching-stage time and stopping-CT time.
-setClass("CTData",representation(PatsData="list",CTTimes="OptionalNumeric",Conclusions="NumericLogical"),
-    prototype=list(CTTimes=NULL),
-    validity=function(object){
-        if (length(object@PatsData)==0) TRUE
-        else{
-            if(! all(Check<-sapply(object@PatsData, function(x) is(x,"PatData"))))
-                stop("The following elements are not objects of the PatData class:","\n",paste(which(!Check), collapse=","),"\n" )
-            else TRUE
-        }
-    }
+setClass("CTData",
+         representation(PatsData="list",CTTimes="OptionalNumeric",Conclusions="NumericLogical"),
+         prototype=list(CTTimes=NULL),
+         validity=function(object){
+           if (length(object@PatsData)==0) TRUE
+           else{
+             if(! all(Check<-sapply(object@PatsData, function(x) is(x,"PatData"))))
+               stop("The following elements are not objects of the PatData class:","\n",paste(which(!Check), collapse=","),"\n" )
+             else TRUE
+           }
+         }
 )
         
 ### Classes and Methods for Action Queue
@@ -116,12 +126,22 @@ setMethod("addAction",signature(currentActionQ="ActionQueue",newAction="Action")
 ### Classes and Methods for population model
 ## Class: BaseCharModelSpecifier
 setClass("BaseCharModelSpecifier",
+         contains="Specifier",
     representation(BaseCharName="character",
                    ConditionBaseCharNames="OptionalCharacter",
                    RGenFun="character"),
-    prototype = list(ConditionBaseCharNames = NULL),
-         contains="Specifier")
-
+    prototype = list(BaseCharName="noName", 
+                     ConditionBaseCharNames = NULL,
+                     RGenFun="NA")
+)
+setMethod("initialize", signature=signature("BaseCharModelSpecifier"),
+          function(.Object, ...) {
+            .Object=callNextMethod(.Object, ...)
+            .Object@parameters$BaseCharName = .Object@BaseCharName
+#            .Object@requirements = c(.Object@requirements, .Object@ConditionBaseCharNames)
+            .Object@provisions = c(.Object@provisions, .Object@BaseCharName)  
+            .Object
+  })
 ## Method: generateBaseChar  
 # this method can only be dispatched within the method "generateBaseChars"  
 setGeneric("generateBaseChar",
