b9593b391b0f950363b80b94c42a6a2eeec6cc63
Roger Day <notsofeo@gmail.com>
2013-06-18 06:08:20 -0400

    Removed misleading startup message.

--- CTDesignExplorer/R/zzz.R
+++ CTDesignExplorer/R/zzz.R
@@ -3,6 +3,5 @@
 	desc <- packageDescription(pkgname)
 	packageStartupMessage("This is ", pkgname, " ", desc$Version, " ", desc$Date)
 	#	createCTDEcatalog()    # Can't write into locked env.
-  cat("Run createCTDEcatalog() to get started.\n")
 	return(invisible(NULL))
 }
