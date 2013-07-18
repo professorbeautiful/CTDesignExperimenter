417d70794943c612053f7546df9ded291696e820
Roger Day <notsofeo@gmail.com>
2013-07-17 20:43:29 -0400

    Using print method defined in VariableGenerator.R --- Typically we will NOT assign the return value to an R object, except temporarily in evaluateOutput().

--- CTDesignExplorer/R/variables.R
+++ CTDesignExplorer/R/variables.R
@@ -1,13 +1,16 @@
 setClass("Variable", contains="character",
-         representation=representation(description="character", 
+         slots=list(description="character", 
                                        dataType="character",
                                        dataTypeDetail="ANY"),
          prototype=prototype(description="This is my string variable", 
                              dataType="character")
 )
 ## This will allow you to access a variable's value like this:
-##   sex = "M"
-###  get(new("Variable", "sex", description="my sex", dataType="factor"))
+   sex = "F"
+  get(new("Variable", "sex", description="my sex", dataType="factor"))
+###  So its "contains" can be a value!  Not what's intended though.
+  sex = "theSexVariable"
+### Potentially very confusing.
 
 ### Later we can change dataType to a function that validates a value.
 
@@ -25,10 +28,10 @@ clearanceRateVariable = new("Variable", "clearanceRateVariable", description="ge
 ############################
 
 setClass("VariableValue", contains="character",
-         representation=representation(variable="Variable", value="ANY"),
+         slots=list(variable="Variable", value="ANY"),
          prototype=prototype(variable=sexVariable, value="Male"),
          validity=function(object){
-           if(is(object@value, object@variable@dataType))
+           if(is(object=object@value, class2=object@variable@dataType))
              return(TRUE)
            #We should also check,if it's a factor, that the value is one of the
            #levels of that factor.
@@ -39,7 +42,10 @@ setClass("VariableValue", contains="character",
 )
 # new("VariableValue")
 ageValue = new("VariableValue", variable=ageVariable, value=94)
-clearanceRateValue = new("VariableValue", variable=clearanceRateVariable, value=1)@variable@.Data
+clearanceRateValue = new("VariableValue", variable=clearanceRateVariable, value=1)
+clearanceRateValue@variable@.Data
+clearanceRateValue@value@.Data
+clearanceRateValue@value
 
 ############################################################
 #' @title   A component of a PopulationModel, generating just one variable value.
@@ -52,103 +58,14 @@ clearanceRateValue = new("VariableValue", variable=clearanceRateVariable, value=
 #'   \item{\code{outputVariable}:}{An object of class \code{Variable}}
 #'   \item{\code{generatorCode}:}{A function calculating the value for the variable.}
 #'   }}
-setClass("VariableGenerator", contains="Specifier",
-         representation=representation(
-           outputVariable="Variable"
-            ### this will be the provision. At first, a string. Later, a Variable.
-           , generatorCode="function" 
-           ### Arguments are the requirements,
-           ### which are VariableValues.
-         ),
-         validity=function(object) { # has to be "object"
-           if(length(object@requirements)==0) 
-             return(TRUE)## For now.
-           else {
-             for(req in object@requirements){
-               if(!is("VariableGenerator", req))
-                 return(paste0("VariableGenerator: error: ",
-                               "req not a VariableGenerator:\n",
-                               req))
-             }
-             return(TRUE)
-           }
-         }
-)
-#setClass("DependentVariableGenerator", contains="SimpleVariableGenerator",
-  ###  Take each "requirement", which is a VariableGenerator,
-         # and recursively generates its value.
-         # Then
-}
-### We should not need separate DependentVariableGenerator and SimpleVariableGenerator
-getVariableValues = function(generator){
-  if(is.null(generator@requirements))
-    return(list(evaluateOutput(generator)))
-  input = sapply(generator@requirements, getVariableValues)
-  output = evaluateOutput(generator, input)
-  return(c(input, output))
-    ### Combine the generator's inputs with its output variable.
-}
-         
-clearanceRate = new("VariableGenerator",
-                    parameters=list(location=15, sdev=0.5),
-                    provisions=list("clearanceRate"),
-                    outputVariable=clearanceRateVariable,
-                    #outputName="clearanceRate", 
-                    ### Maybe we don't need outputName? Use the object name, right?
-    generatorCode=function() {
-      #We don't really need Multiplier. Just testing out the parameter idea.
-#      if(missing(Multiplier)) Multiplier = 1
-      return(exp(rnorm(1,mean=log(location), sd=sdev))
-      )
-    }
-)
-
-#' Evaluate a generator expression.
-#' 
-#' \code{evaluateOutput} 
-#' \param generator A SimpleVariableGenerator 
-#' \param input Input "requirements" list of VariableValue objects.
-evaluateOutput = function(generator, input) {
-  ## Make the generatorCode function available.
-  if(!is(generator, "VariableGenerator"))
-    stop("evaluateOutput: generator should be a VariableGenerator")
-  ## Make the generator itself visible.
-  inputs = as.environment(list(theGeneratorCode=generator@generatorCode))
-  ## Make the parameters available.
-  inputs = list2env(generator@parameters, inputs)
-  if(!missing(input)){
-    ## Make the inputs (requirements) available.
-    if(!is.list(input)) { ### input is a single variable
-      input = list(input)
-      if(length(extractRequirements(generator))==1)
-        names(input) = extractRequirements(generator)
-      else
-        stop("evaluateOutput: input is not a list, but the requirements length is not 1.")
-    }
-    inputs = list2env(input, inputs)
-    command = paste0("theGeneratorCode(",
-                     paste(names(input), collapse=", "),
-                     ")")
-  }
-  else
-    command = "theGeneratorCode()"
-  #  print(sys.call())
-  catn("command:", command)
-  print(ls(env=inputs))
-  catn("inputs: \n")
-  print(inputs)
-  catn("is.list(inputs)", is.list(inputs))
-#  environment(inputs$theGeneratorCode) = inputs
-  environment(generator@generatorCode) = 
-    list2env(generator@parameters, environment(generator@generatorCode))
-  value = generator@generatorCode()
-#  value = eval(parse(text="inputs$theGeneratorCode()"))
-  print(value)
-  print(generator@outputVariable)
-  return(new("VariableValue", variable=generator@outputVariable, value=value))
-}
-
-evaluateOutput(clearanceRate)
+# getVariableValues = function(generator){
+#   if(is.null(generator@requirements))
+#     return(list(evaluateOutput(generator)))
+#   input = sapply(generator@requirements, getVariableValues)
+#   output = evaluateOutput(generator, input)
+#   return(c(input, output))
+#     ### Combine the generator's inputs with its output variable.
+# }
 
 
 # setMethod("generatePatientFeatures",
@@ -157,55 +74,32 @@ evaluateOutput(clearanceRate)
 #             evaluateOutput(variableGenerator)
 #           }
 # )
-setMethod("generateBaseChar",
-          signature(baseCharModelSpec="BaseCharModelSpecifier"),
-          function(baseCharModelSpec){
-evaluateOutput(clearanceRate,0)
-evaluateOutput(clearanceRate) ## OK works if no input provided.
 
 # list2env: Since environments are never duplicated, the argument envir is also changed.
 # GOOD get("A", env=as.environment(list(A=10)))
 # GOOD eval(expression(A), env=as.environment(list(A=10)))
 
 extractRequirements = function(generator){
-  formalArgs(generator@generatorCode)
+#  formalArgs(generator@generatorCode)
+  generator@requirements
 }
 extractRequirements(clearanceRate)
 
-extractProvisions = function(generator){
-  generator@outputName
-}
-extractProvisions(clearanceRate)
+# extractProvisions = function(generator){
+#   generator@outputName
+# }
 
-##### ok to here.########
-toxDoseThreshold  = new("SimpleVariableGenerator",
-                        parameters=list(location=15, sdev=0.5),
-                        requirements=list(),
-                        outputName="toxDoseThreshold", 
-                        ### But outputName might as well be the object name, right?
-                        generatorCode=function(B) { ## B just to test conditional.
-                          PKclearance * 
-                            exp(rnorm(baseCharModelSpec@location,
-                                      sd=baseCharModelSpec@sd)))
-)))
-                        }
-)
+# extractProvisions(clearanceRate)
+#  NOT SURE ABOUT THIS.
+# For a VariableGenerator,
+# the name of the generator is the provision.
+# For a VariableGenerator, or PopulationModel,
+# it's the c() of its VariableGenerators.
 
-
-setClass(Class="ToxDoseThresholdModel",
-         contains="BaseCharModelSpecifier",
-         representation=representation(
-           location="numeric", sd="numeric"),
-         prototype=prototype(
-           ConditionBaseCharNames = "PKclearance",
-           location=1, sd=0.02,
-           RGenFun="PKclearance * exp(rnorm(baseCharModelSpec@location, sd=baseCharModelSpec@sd))")
-)
-
-standardToxDoseThresholdModel = new("ToxDoseThresholdModel",
-                                    BaseCharName = "ToxDoseThreshold") 
 
 
 
 setClass("VariableNetwork", contains="Specifier")
 ###  for combining variables into a patient description model.
+### -- or call it "VariableGeneratorBundle" ???
+# Then PopulationModel can be a subclass of VariableNetwork.
