52b08ed5c10292cd29994f9e99a2868f4bf169d5
Roger Day <notsofeo@gmail.com>
2013-08-26 20:45:39 -0400

    Moved createCTDEcatalog.R to inst/helperCode

--- CTDesignExplorer/inst/helperCode/createCTDEcatalog.R
+++ CTDesignExplorer/inst/helperCode/createCTDEcatalog.R
@@ -16,10 +16,37 @@ clear = function(){
   }
 }
 
+createVariableCatalog = function() {
+  v_sexVariable = new("Variable", name="sex", 
+                      description="my sex variable, as an unrestricted string", dataType="character")  
+  v_ageVariable = new("Variable", name="age", 
+                      description="age, as an unrestricted number", dataType="numeric")
+  v_ageCategoryVariable = new("Variable", name="age", 
+                              description="age, binned by decade", 
+                              dataType="factor",
+                              dataTypeDetail=paste0("(", 10*(0:9), ",", 10*(1:10), "]"))
+  v_clearanceRate = new("Variable", name="clearanceRate", description="generic clearance rate variable",
+                        dataType="numeric")
+  v_toxDoseThreshold = new("Variable", name="toxDoseThreshold", description="dose threshold for binary toxicity event",
+                           dataType="numeric")
+  v_responseDoseThreshold = new("Variable", name="responseDoseThreshold", description="dose threshold for binary response event",
+                                dataType="numeric")  
+}
+
+### Tests:
+# new("VariableValue", 94, variable=v_ageVariable)
+# try(silent=TRUE,
+#     new("VariableValue", "xyz", variable=v_ageVariable)) ### fail
+
+createVariableGeneratorCatalog = function() {
+  
+}  
+
 createCTDEcatalog = function() {
   ### Based on rogerTestsYuanyuanCode.R
   #print(ls())  ### Initially, nothing.
-  local(envir=.GlobalEnv, expr={    
+  local(envir=.GlobalEnv, expr={ 
+    
     threePlusThree_five_tiers = 
       new("APlusBSpecifier", TierDoses=1:5)
     crm9 <- new("CRMSpecifier",
