7eb34e1f082a2997941783f47ab208fc517e7dc9
Roger Day <notsofeo@gmail.com>
2013-08-26 22:48:53 -0400

    Added createVariableCatalog as a function. I think the local{} in createCTDEcatalog might be better.

--- CTDesignExplorer/R/createVariableCatalog.R
+++ CTDesignExplorer/R/createVariableCatalog.R
@@ -1,4 +1,4 @@
-#createVariableCatalog = function() {
+createVariableCatalog = function() {
   v_sexVariable = new("Variable", name="sex", 
                       description="my sex variable, as an unrestricted string", dataType="character")  
   v_ageVariable = new("Variable", name="age", 
@@ -13,6 +13,7 @@
                            dataType="double")
   v_responseDoseThreshold = new("Variable", name="responseDoseThreshold", description="dose threshold for binary response event",
                                 dataType="double")  
-#}
+  for(v in ls()) assign(v, get(v), pos=1)
+}
 
 #createVariableCatalog()
