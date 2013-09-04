57021748f93dedac6c9fec3829c7f63b3b3f4997
Roger Day <notsofeo@gmail.com>
2013-09-04 11:03:49 -0400

    runScenario as a call to sim1CT.

--- CTDesignExplorer/R/scenarios.R
+++ CTDesignExplorer/R/scenarios.R
@@ -1,3 +1,12 @@
 cat("======== scenarios.R  ================\n")
 
-setClass("Scenario", contains="VariableNetwork")
+setClass("Scenario", contains="VariableNetwork",
+         slots=list(popModel="PopulationModel",
+                    design="Design",
+                    outcomeModel="OutcomeModel"))
+
+### sim1CT
+
+runScenario = function(scenario){
+  sim1CT(scenario@popModel, scenario@design, scenario@outcomeModel)
+}
