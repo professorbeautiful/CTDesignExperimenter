82760f0330558e0a8b96aa9597a1b4528c4f815a
Roger Day <notsofeo@gmail.com>
2013-09-24 15:26:05 -0400

    fun with "double" -  OBOY
    strange things about numeric classes... and not.

--- CTDesignExplorer/inst/helperCode/typeof_class_as_is_for_numerics.R
+++ CTDesignExplorer/inst/helperCode/typeof_class_as_is_for_numerics.R
@@ -1,23 +1,36 @@
 typeof(7)
 #[1] "double"
-
 is(7, "numeric")
 #[1] TRUE
-
-class(7)
-#[1] "numeric"
-
-is(7, "double")
-#[1] FALSE
 is(7, "integer")
-#[1] FALSE
+# [1] FALSE
+is.integer(7)
+# [1] FALSE
+is(7, "double")
+# [1] FALSE
+is.double(7)
+# [1] TRUE
+# Hmm, a little confusing.
+
+identical(as(7, "double"), 7)
+# [1] TRUE
 
 is(as(7, "double"), "double")
 # [1] FALSE
-# OBOY
+#OBOY. Hard to explain this to my python-loving friends.
 
-class(as(7, "double"))
-#[1] "numeric
+# So "double" is not a class:
+showClass("double")
+# Error in getClass(Class) : “double” is not a defined class
 
-typeof(7L)
-[1] "integer"
+# So, what does as(7, "double") do?
+# Shouldnt it throw an error, since the Class arg is not a class name?
+  class(as(7, "double"))
+  #[1] "numeric
+# Here is the story, inside as():
+  if (.identC(Class, "double")) 
+    Class <- "numeric"
+
+showClass("numeric")
+So we learn that unlike "double", "numeric" is a class; 
+it has 2 subclasses:  integer and ordered. But not double.
