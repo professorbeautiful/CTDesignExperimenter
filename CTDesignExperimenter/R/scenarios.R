e714461b588a10e561315e21ee763cafd7927cb1
Roger Day <notsofeo@gmail.com>
2013-12-30 01:10:45 -0500

    Moved lots out of Scenario.R.  Maybe nothing here is needed.

--- CTDesignExperimenter/R/scenarios.R
+++ CTDesignExperimenter/R/scenarios.R
@@ -18,33 +18,6 @@ cat("======== scenarios.R  ================\n")
 #' 
 
 #####
-setClass("Insert", contains="Specifier")
-setClass("InsertVariable", contains=c("Insert", "VariableGenerator"))
-setClass("InsertEvent", contains=c("Insert", "EventGenerator"))
-
-
-setClass("PatientAttribute", contains="InsertVariable")
-setClass("EligibilityCriterion", contains="InsertVariable")
-setClass("ScheduleTreatment", contains="InsertEvent")
-setClass("OffStudyCriterion", contains="InsertVariable")
-setClass("ModificationRule", contains="InsertEvent")
-setClass("PatientSummary", contains="InsertVariable")
-setClass("StoppingCriterion", contains="InsertEvent")
-setClass("TrialSummary", contains="InsertVariable")
-
-#' ListOfInserts
-#' 
-#' A list of Insert objects.
-#' Generally the parameters are the defaults.
-#' When a new Scenario is defined, the parameters may be changed.
-setClass("ListOfInserts", contains="list",
-         validity=function(object) {
-           if(length(ob)==0) return(TRUE)
-           whichAreInserts = sapply(object, is, "Insert")
-           if(all(whichAreInserts)) return(TRUE)
-           return(paste("ERROR in ListOfInserts: ", which(!whichAreInserts)))
-})
-
 
 
 #' Scenario
@@ -54,13 +27,10 @@ setClass("ListOfInserts", contains="list",
 #' An evaluation will consist of a list of scenarios, together with evaluation criteria.
 #'  The scenarioEnv holds inserts (with parameter values assigned), 
 #'  variablevalues, and patient data.
-setClass("Scenario", contains=ListOfInserts,
-                    scenarioEnv="environment"))
+setClass("Scenario", contains="ListOfInserts")
 #####
 
+setClass("CTSimulation", slots=list(
+    scenario="Scenario",
+    scenarioEnv="environment"))
 
-### sim1CT  #####
-
-runScenario = function(scenario){
-  sim1CT(scenario@popModel, scenario@design, scenario@outcomeModel)
-}
