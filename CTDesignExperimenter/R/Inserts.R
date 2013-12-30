8212f0bded09a7a459463d0cb738ba29bb33a0c8
Roger Day <notsofeo@gmail.com>
2013-12-30 01:08:03 -0500

    Defining all the Insert types, ListOfInserts, and some example inserts for testing the queue.

--- CTDesignExperimenter/R/Inserts.R
+++ CTDesignExperimenter/R/Inserts.R
@@ -1,10 +1,13 @@
-cat("Inserts.R")
+cat("======== Inserts.R  ================\n")
 
 #####
+#' Event
 setClass("Event",    ### Like a Variable
          slots=list(name="character", description="character"))
-### Should here be  a time-to-event name?
+### Should there be  a time-to-event name?
 
+#####
+#' Class EventGenerator
 setClass("EventGenerator",    ### Like a VariableGenerator
          contains="Specifier",
          slots=list(
@@ -21,6 +24,8 @@ setClass("EventGenerator",    ### Like a VariableGenerator
            return(TRUE)
          }
 )
+#####
+#' function EventGenerator
 EventGenerator = function(parameters=list(), provisions, 
                              requirements=NULL,
                              outputEvent, generatorCode) {
@@ -39,15 +44,80 @@ EventGenerator = function(parameters=list(), provisions,
   vg
 }
 
+setClassUnion("Action", c("Event", "Variable"))
+setClassUnion("ActionGenerator", c("EventGenerator", "VariableGenerator"))
+
+
+setClass("Insert", contains="Specifier")
+setClass("InsertVariable", contains=c("Insert", "VariableGenerator"))
+setClass("InsertEvent", contains=c("Insert", "EventGenerator"))
+
+
+setClass("PatientAttribute",      contains="InsertVariable")
+setClass("EligibilityCriterion",  contains="InsertVariable")
+setClass("ScheduleTreatment",     contains="InsertEvent")
+setClass("OffStudyCriterion",     contains="InsertVariable")
+setClass("ModificationRule",      contains="InsertEvent")
+setClass("PatientSummary",        contains="InsertVariable")
+setClass("StoppingCriterion",     contains="InsertEvent")
+setClass("TrialSummary",          contains="InsertVariable")
+
+#' ListOfInserts
+#' 
+#' A list of Insert objects.
+#' Generally the parameters are the defaults.
+#' When a new Scenario is defined, the parameters may be changed.
+setClass("ListOfInserts", contains="list",
+         validity=function(object) {
+           if(length(object)==0) return(TRUE)
+           whichAreInserts = sapply(object, is, "Insert")
+           if(all(whichAreInserts)) return(TRUE)
+           return(paste("ERROR in ListOfInserts: ", which(!whichAreInserts)))
+         })
+
+
+##### EligibilityCriteria ##### 
 setValidity(Class="EligibilityCriterion", 
          method=function(object) {
            provision = slot(object, name="provisions")
-           if(!is(provision, "Variable"))
+           if(!is(provisions, "Variables"))
              return("eligibilityCriterion provision is not a Variable")
            if(identical(slot(object, provision)@checkDataType, is.logical))
              return("checkDataType should be is.logical")
            return(TRUE)
          }
 )
+v_ageVariable
+vg_age = as(Class="PatientAttribute",
+            VariableGenerator(parameters=list(ageMean=50, ageSD=10),
+                              generatorCode=function(){
+                                rnorm(1, ageMean, ageSD)
+                              },
+                              provisions=v_ageVariable,
+            ))
+ec_age = as(Class="EligibilityCriterion", VariableGenerator( parameters=list(cutoff=50),
+                  requirements=VariableList(v_ageVariable),
+                  outputVariable=Variable(name="isOldEnough",
+                                          description="Patient is old enough",
+                                          checkDataType=is.logical),
+                  generatorCode=function()  { age >= cutoff}
+))  #### OK.
+v_liverVariable = Variable(name="liverFunction", desc="Liver function", 
+                           gitAction="push",
+                           checkDataType=function(x) is.numeric(x))
+vg_liver = as(Class="PatientAttribute",
+            VariableGenerator(parameters=list(liverMean=1, liverSD=0.2),
+                              generatorCode=function(){
+                                rnorm(1, liverMean, liverSD)
+                              },
+                              provisions=v_liverVariable
+            ))
+ec_liver = as(Class="EligibilityCriterion", 
+              VariableGenerator( parameters=list(cutoff=1.5),
+                                 requirements=VariableList(v_liverVariable),
+                                 outputVariable=Variable(name="liverOK",
+                                                         description="Patient has sufficient liver function.",
+                                                         checkDataType=is.logical),
+                                 generatorCode=function()  { liverFunction <= cutoff}
+              ))  #### OK.
 
-new("EligibilityCriterion", )
