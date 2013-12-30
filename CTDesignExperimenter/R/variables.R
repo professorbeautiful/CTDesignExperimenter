68c298167476ca1d00cd9c8580ca6f16af4a9138
Roger Day <notsofeo@gmail.com>
2013-12-30 01:16:03 -0500

    Remove writeVariableFile etc to gitUtilities.

--- CTDesignExperimenter/R/variables.R
+++ CTDesignExperimenter/R/variables.R
@@ -113,30 +113,6 @@ VariableValue = function(value, variable) {
 # )
 # new("VariableValue")
 
-#writeVariableFile = function(name, description, checkDataType, checkDataTypeDetail="", 
-writeVariableFile = function(variable, 
-                             dir="../CTDEswapmeet",
-                             author=system("echo $USER",intern=TRUE),
-                             time=Sys.time()
-                             ){
-  name = variable@name
-  description = variable@description
-  checkDataType = variable@checkDataType
-  filename = paste("v_", name, ".", as.numeric(time), ".R", sep="")
-  dput(variable, file=paste0(dir, "/",filename))
-  commitVariableFile(filename, dir)
-}
-
-commitVariableFile = function(fileName, dir="../CTDEswapmeet", comment) {
-  if(missing(comment))
-    comment = readline("Please write a comment for git (one line only): ")
-  system(paste0("cd ", dir, "; git add ", fileName, 
-                "; git commit ", fileName, " -m'", comment, "'"))
-}
-
-pushVariables = function(dir="../CTDEswapmeet") {
-  system(paste0("cd ", dir, "; git push"))
-}
 
 v_liverVariable = Variable(name="liverFunction", desc="Liver function",
                            gitAction="push",
