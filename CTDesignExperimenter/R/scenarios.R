e0b1b6219c1cbdb29c6be85cf266f76264138c60
Roger Day <notsofeo@gmail.com>
2014-01-05 19:02:10 -0500

    Some #####'s added.

--- CTDesignExperimenter/R/scenarios.R
+++ CTDesignExperimenter/R/scenarios.R
@@ -26,10 +26,11 @@ cat("======== scenarios.R  ================\n")
 #' 
 #' An evaluation will consist of a list of scenarios, together with evaluation criteria.
 #'  The scenarioEnv holds inserts (with parameter values assigned), 
-#'  variablevalues, and patient data.
+#'  variablevalues, and patient data. #####
 setClass("Scenario", contains="ListOfInserts")
 #####
-
+##' extractParameters #####
+##' We will need this to construct interfaces.
 extractParameters = function(obj){
   if(is(obj, "list"))
     lapply(obj, slot, name="parameters")
