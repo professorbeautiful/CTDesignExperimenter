85d120b650d5209ead4c3e02846b05c3a6d25b9e
Roger Day <notsofeo@gmail.com>
2014-01-05 22:22:21 -0500

    New fixes needed to assure successful build. Removed setValidity(Class="EligibilityCriterion", since we no longer have this class. RE-did construction of vg_age etc to reflect the new constructors, PatientAttribute() etcetera.

--- CTDesignExperimenter/R/Inserts.R
+++ CTDesignExperimenter/R/Inserts.R
@@ -76,41 +76,40 @@ validity_ListOfInserts = function(object) {
 setValidity(Class="ListOfInserts", validity_ListOfInserts)
 
 ##### EligibilityCriteria ##### 
-setValidity(Class="EligibilityCriterion", 
-         method=function(object) {
-           provision = slot(object, name="provisions")
-           if(!is(provisions, "Variables"))
-             return("eligibilityCriterion provision is not a Variable")
-           if(identical(slot(object, provision)@checkDataType, is.logical))
-             return("checkDataType should be is.logical")
-           return(TRUE)
-         }
-)
+# TODO: move this to VariableGenerator.
+# setValidity(Class="EligibilityCriterion", 
+#          method=function(object) {
+#            provision = slot(object, name="provisions")
+#            if(!is(provisions, "Variables"))
+#              return("eligibilityCriterion provision is not a Variable")
+#            if(identical(slot(object, provision)@checkDataType, is.logical))
+#              return("checkDataType should be is.logical")
+#            return(TRUE)
+#          }
+# )
  
-vg_age = as(Class="PatientAttribute",
-            VariableGenerator(parameters=list(ageMean=50, ageSD=10),
+vg_age = PatientAttribute(parameters=list(ageMean=50, ageSD=10),
                               generatorCode=function(){
                                 rnorm(1, ageMean, ageSD)
                               },
-                              provisions=v_ageVariable,
-            ))
-ec_age = as(Class="EligibilityCriterion", VariableGenerator( parameters=list(cutoff=50),
+                              provisions=v_ageVariable
+)
+ec_age = EligibilityCriterion( parameters=list(cutoff=50),
                   requirements=VariableList(v_ageVariable),
                   outputVariable=Variable(name="isOldEnough",
                                           description="Patient is old enough",
                                           checkDataType=is.logical),
                   generatorCode=function()  { age >= cutoff}
-))  #### OK.
+)  #### OK.
 v_liverVariable = Variable(name="liverFunction", desc="Liver function", 
                            gitAction="none",
                            checkDataType=function(x) is.numeric(x))
-vg_liver = as(Class="PatientAttribute",
-            VariableGenerator(parameters=list(liverMean=1, liverSD=0.2),
+vg_liver = PatientAttribute(parameters=list(liverMean=1, liverSD=0.2),
                               generatorCode=function(){
                                 rnorm(1, liverMean, liverSD)
                               },
                               provisions=v_liverVariable
-            ))
+            )
 ec_liver = EligibilityCriterion( parameters=list(cutoff=1.5),
                                  requirements=VariableList(v_liverVariable),
                                  outputVariable=Variable(name="liverOK",
