4c50c54727eca9fa9001109ebe0e5c55fe7b6328
Roger Day <notsofeo@gmail.com>
2014-01-06 20:43:48 -0500

    Moving collateOrder.R to R folder; wrapping everything in function defs.

--- CTDesignExperimenter/R/collateOrder.R
+++ CTDesignExperimenter/R/collateOrder.R
@@ -6,13 +6,15 @@ collateOrder = function(){
                         stringsAsFactors=F))[[1]]))[[1]][-1]
 }
 
-
+runCollatedFiles = function(){
 for(f in collateOrder()) { 
   cat(".............", f, "...........\n"); 
   source(paste0("CTDesignExperimenter/R/",f), keep.source=TRUE)
   ### keep.source does nothing useful for trace()
-}
+}}
 
-setdiff = function(In, notIn) { base:::setdiff(x=In, y=notIn)} ## so we know what's what.
-setdiff(In=collateOrder(), notIn=dir(path="CTDesignExperimenter/R")) 
-setdiff(notIn=collateOrder(), In=dir(path="CTDesignExperimenter/R"))
+checkCollation = function(){
+  setdiff = function(In, notIn) { base:::setdiff(x=In, y=notIn)} ## so we know what's what.
+  setdiff(In=collateOrder(), notIn=dir(path="CTDesignExperimenter/R")) 
+  setdiff(notIn=collateOrder(), In=dir(path="CTDesignExperimenter/R"))
+}
