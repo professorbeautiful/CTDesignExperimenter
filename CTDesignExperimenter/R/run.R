c3cc96b9b9d17873746bf0432de4976b200536b7
Roger Day <notsofeo@gmail.com>
2014-03-06 10:35:42 -0500

    Added important {}, lined 13 to 21. And indentation fixes.

--- CTDesignExperimenter/R/run.R
+++ CTDesignExperimenter/R/run.R
@@ -3,20 +3,21 @@ cat("======== run  ================\n")
 
 run = function(background=FALSE, shinyfolder="shinyScenario", local=FALSE, createCTDEcatalog=FALSE) {
   if(createCTDEcatalog) createCTDEcatalog()
-#   if(background) { ## Run it in the background outside of this session.
-#     system("echo 'library(CTDesignExplorer); run()' | R --vanilla")
+  #   if(background) { ## Run it in the background outside of this session.
+  #     system("echo 'library(CTDesignExplorer); run()' | R --vanilla")
   #   PROBLEMS!  Warning in .simpleDuplicateClass(def, prev) :
-#  the specification for S3 class “AsIs” in package ‘BiocGenerics’ seems equivalent to one from package ‘RJSONIO’ and is not turning on duplicate class definitions for this class
-#   }
+  #  the specification for S3 class “AsIs” in package ‘BiocGenerics’ seems equivalent to one from package ‘RJSONIO’ and is not turning on duplicate class definitions for this class
+  #   }
   result = tryCatch( error = function(e) e,
-    expr=require("shiny"))
-  if(class(result) != "try-error")
+                     expr=require("shiny"))
+  if(class(result) != "try-error"){
     if(local)
-      runApp(paste0("CTDesignExperimenter/inst/", shinyfolder)
-  else
-    runApp(system.file(
-      package="CTDesignExperimenter", shinyfolder))
-  ### NOTE that the folder inst/shinydocs 
-  ### becomes just shinydocs after building.
+      runApp(paste0("CTDesignExperimenter/inst/", shinyfolder))
+    else
+      runApp(system.file(
+        package="CTDesignExperimenter", shinyfolder))
+    ### NOTE that the folder inst/shinydocs 
+    ### becomes just shinydocs after building.
+  }
   return(invisible(NULL))
 }
