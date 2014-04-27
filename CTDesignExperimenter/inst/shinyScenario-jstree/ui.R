5685a583ec53a2629bdb11cb2f69643cf56d2e3d
Roger Day <notsofeo@gmail.com>
2014-04-27 11:59:18 -0400

    A couple of successful ways of opening all tree nodes.

--- CTDesignExperimenter/inst/shinyScenario-jstree/ui.R
+++ CTDesignExperimenter/inst/shinyScenario-jstree/ui.R
@@ -2,20 +2,42 @@
 ### See /Users/Roger/Library/R/3.0/library/shinysky/examples/6_jstree/
 
 require(shinysky)
-
+"%&%" = function (a, b)   paste(a, b, sep = "") 
 scenarioTree = makeTree("full")
 
 # nav = list(Branch = list(twig = list("leafOnTwigA", "leafOnTwigB"), 
 #                          "leafOnBranch"), 
 #            "Topleaf")
 
-shinyUI(basicPage(headerPanel("shinyScenario-jstree"), br(), br(),
-                  h4("Treeview (based on jstree)"), 
-                  div(class = "well container-fluid", 
-                      div(class = "row-fluid", 
-                          div(class = "row-fluid",
-                              div(class = "span2", 
-                                  jstree("jstree1", jstree.obj(scenarioTree))), 
-                              div(class = "span10", shinyalert("alert_jstree1"))
-                              ))
-                      )))
+myTreeObj = jstree.obj(scenarioTree)
+# myTreeObj[["children"]][[1]][[11]][[1]] = 
+#   'li class="jstree-open"'
+#myTree = jstree.local("jstree1", myTreeObj, '.jstree("open_all")')
+myTree = jstree.local("jstree1", myTreeObj, '')
+# Fails.  The closing tag is also set to class="jstree-open"
+# Must be a special [[<- method.
+shinyUI(
+  basicPage(
+    headerPanel("shinyScenario-jstree"), 
+    br(), br(),
+    h4("startup 3"), 
+    div(class = "well container-fluid", 
+        div(class = "row-fluid", 
+            div(class = "row-fluid",
+                div(class = "span2", 
+                    myTree) , 
+                div(class = "span10", 
+                    shinyalert("alert_jstree1"))
+            ),
+            hr(),
+            ### Seems to have to be at the end.
+            tags$script(
+              #  '$("#jstree1").open_all();'
+              'function openTree(){
+                            $("#jstree1").jstree("open_all");
+                            };
+              $(document).ready(openTree);')
+        )
+    )
+  )
+)
