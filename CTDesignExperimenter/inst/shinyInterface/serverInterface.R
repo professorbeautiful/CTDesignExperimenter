18a3787a9976e992323fbca545e4850e62143908
Roger Day <notsofeo@gmail.com>
2014-05-23 00:39:03 -0400

    Still trying to use observe(), without success.

--- CTDesignExperimenter/inst/shinyInterface/serverInterface.R
+++ CTDesignExperimenter/inst/shinyInterface/serverInterface.R
@@ -14,6 +14,9 @@ shinyServer(function(input, output, session) {
   treeObserver = observe(
     label="myTreeObserver",
     {
+      cat("Entered treeObserver. #selected nodes =",
+        length(input$jstree1), "\n")
+#        "depths are ", input$jstree1)
       showshinyalert(session, "alert_jstree1", 
                    paste0("You selected these items in the tree: ", 
                           paste0(input$jstree1, collapse = ", ")))
@@ -21,6 +24,7 @@ shinyServer(function(input, output, session) {
     }
   )
   # treeObserver$onInvalidate(function() print("jstree1 selection changed!"))
+
   output$selectedNode = renderText({
     print(paste0(input$jstree1, collapse = ", "))
     })
