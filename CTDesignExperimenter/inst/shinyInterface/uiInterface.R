547b68667ddd3c38f2cc8959eb9d402e15db23b4
Roger Day <notsofeo@gmail.com>
2014-05-23 00:40:24 -0400

    Moved conditional panels out, and changed some button names.

--- CTDesignExperimenter/inst/shinyInterface/uiInterface.R
+++ CTDesignExperimenter/inst/shinyInterface/uiInterface.R
@@ -12,50 +12,55 @@ tagToOpenTree =
     , '                   };
        $(document).ready(openTree);'))
 
+conditionPanel1 = conditionalPanel(condition = #'$( "select option:selected".length > 0 )',
+                                     #  '!(typeof input.jstree1 === "undefined") && input.jstree1.length > 0',
+                                     #  'output.numberSelected', ## fails
+                                     #   'output.selectedNode', # this works!
+                                     'input.jstree1.length == 1', #better
+                                   hr(),
+                                   actionButton(inputId="btnRemoveInsert" , label="Remove insert", styleclass = "success"),
+                                   actionButton(inputId="btnCloneInsert" , label="Clone insert", styleclass = "success"),
+                                   actionButton(inputId="btnEditInsert" , label="Edit insert", styleclass = "success"),
+                                   textOutput("selectedNode"),
+                                   #                                           textOutput("nSelectedText"),
+                                   hr())
+conditionPanelMoreThan1 = 
+  ###  Buttons when > 1 items are selected:
+  conditionalPanel(condition =  'input.jstree1.length > 1',
+                   # 'output.moreThanOneSelected == true', 
+                   hr(),
+                   actionButton(inputId="btnSaveListOfInserts" , label="btnSaveListOfInserts", styleclass = "success"),
+                   hr())
+
 scenarioPanel = tabPanel("scenario",
                          #div(class="row-fluid span1",
                          uiOutput(outputId="debugTools")
                          #                             )
                          ,
                          textInput(inputId="scenarioName",  
-                                   label="scenario name"),
+                                   label="scenario name",
+                                   value=currentScenario@name),
                          div(class="row-fluid span3",
-                             actionButton(inputId="btnRenameScen", label="rename scenario", styleclass = "success"),
-                             actionButton(inputId="btnSaveScen" , label="save scenario", styleclass = "success"),
-                             actionButton(inputId="btnFindScen" , label="find/replace scenario", styleclass = "success"),
-                             actionButton(inputId="btnAddScen" , label="add scenario", styleclass = "success"),
+                             actionButton(inputId="btnCloneScen", label="Clone and save scenario", styleclass = "success"),
+                             actionButton(inputId="btnFindScen" , label="Find &replace scenario", styleclass = "success"),
+                             actionButton(inputId="btnAddScen" , label="Add scenario to experiment", styleclass = "success"),
                              hr()),
-                         textOutput("selectedNode"),
-                         conditionalPanel(condition = #'$( "select option:selected".length > 0 )',
-                                            #  '!(typeof input.jstree1 === "undefined") && input.jstree1.length > 0',
-                                          #  'output.numberSelected', ## fails
-                                             'output.selectedNode', # this works!
-                                          hr(),
-                                          actionButton(inputId="btnCloneInsert" , label="Clone insert", styleclass = "success"),
-                                          actionButton(inputId="btnEditInsert" , label="Edit insert", styleclass = "success"),
-                                          textOutput("nSelectedText"),
-                                          hr()),
-                         ###  Buttons when > 1 items are selected:
-                         conditionalPanel(condition =  'input.jstree1.length > 1',
-                                          # 'output.moreThanOneSelected == true', 
-                                          hr(),
-                                          actionButton(inputId="btnSaveListOfInserts" , label="btnSaveListOfInserts", styleclass = "success"),
-                                          actionButton(inputId="btnEditInsert" , label="Edit insert", styleclass = "success"),
-                                          hr()),
+                         conditionPanel1,
+                         conditionPanelMoreThan1,
                          myTree,
                          tagToOpenTree 
 )
 
 shinyUI(
-  basicPage(
+  navbarPage(
     headerPanel(h4("CTDE: Clinical trial design experimenter")), 
-    div(class = "well container-fluid", 
-        tabsetPanel(id="panelSet", type="pills",
+#    div(class = "well container-fluid", 
+#        tabsetPanel(id="panelSet", type="pills",
                     scenarioPanel,
                     tabPanel("one run", 
                              "Display results from a single CT run for the selected scenario."),
                     tabPanel("criteria"),
                     tabPanel("evaluation"))
-    )
-  )
+#    )
+#  )
 )
