2c20ac41ce3211c3624e20f8e8088124a1380fcf
Roger Day <notsofeo@gmail.com>
2014-05-23 21:38:38 -0400

    Prettify the debugTools; rename evalOutput to evaluatedOutput

--- CTDesignExperimenter/inst/shinyInterface/debugTools.R
+++ CTDesignExperimenter/inst/shinyInterface/debugTools.R
@@ -1,12 +1,12 @@
 ##  For source'ing in serverInterface.R
 
-output$evalOutput = renderText({
+output$evaluatedOutput = renderText({
   ## This might be useful later for up-arrowing through past expressions.
   evalString = isolate(input$evalString)
   ## you have to isolate; otherwise each character typed calls this callback.
-#   if(is.null(rValues$evalStringHistory))
-#     rValues$evalStringHistory = character(0)
-#  rValues$evalStringHistory = c(rValues$evalStringHistory, evalString)
+  #   if(is.null(rValues$evalStringHistory))
+  #     rValues$evalStringHistory = character(0)
+  #  rValues$evalStringHistory = c(rValues$evalStringHistory, evalString)
   
   if(input$evalButton > 0) eval(parse(text=evalString))
 })  
@@ -18,29 +18,34 @@ output$shiny.trace.text = renderText({
   catn("shiny.trace: ", options("shiny.trace")[[1]], 
        " should equal ", input$traceCheckbox);
   "trace=" %&% input$traceCheckbox
-})   #### Not called!!
+})   #### OK this works now.
 
 
 output$debugTools = renderUI({
-  tag("table", list(
-    tag("tr", 
-        list(
-          tag("TD",
-              list(width=120, style="color: blue",
-                   checkboxInput(inputId="traceCheckbox", 
-                                 value=FALSE,
-                                 label=textOutput("shiny.trace.text")
-                   ))),
-          tag("TD", 
-              list(style="color:\"red\"", actionButton("evalButton", "evalButton"))),
-          tag("TD",
-              list(width=10, textInput(inputId="evalString", label="", value="1+1"))),
-          tag("TD", list(style="color:red", HTML("&rarr;"))),
-          tag("TD",
-              list(width=800, `text-align`="right", color="red", uiOutput(outputId="evalOutput")))
+  div(style="background:darkGrey",
+      em(strong("Debugging aids")),
+      tag("table", list(
+        tag("tr", 
+            list(
+              tag("TD",
+                  list(width=120, style="color: blue",
+                       checkboxInput(inputId="traceCheckbox", 
+                                     value=TRUE,
+                                     label=textOutput("shiny.trace.text")
+                       ))),
+              tag("TD", 
+                  list(style="color:\"red\"", actionButton("evalButton", "evalButton"))),
+              tag("TD",
+                  list(width=10, textInput(inputId="evalString", label="", value="1+1"))),
+              tag("TD", list(style="color:red", HTML("&rarr;"))),
+              tag("TD",
+                  list(width=800, 
+                       style='text-align:"right"; color:white', 
+                       uiOutput(outputId="evaluatedOutput")))
+            )
         )
+      )
     )
-  )
   )
 })
 
