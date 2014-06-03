f70b1c182809ac3ce38c2b10f6c51b832c98e273
Roger Day <notsofeo@gmail.com>
2014-06-03 12:43:15 -0400

    studies-of-makeTree. What's the structure of the output?

---
+++ CTDesignExperimenter/inst/shinyInterface/studies-of-makeTree.R
@@ -0,0 +1,16 @@
+
+
+
+pbcopy(capture.output(unlist(myTreeObj)))
+## then paste into TextEdit, and adjust the window width to fix wrapping.
+
+unlist(myTreeObj)[ which(unlist(myTreeObj) == "li") + 4]
+table(names(..()))
+sum(..())  #  67, same as the last node j1_67.
+
+unlist(myTreeObj)[ grep("[0-9]$", names(unlist(myTreeObj)))]
+table(names(..()))  ### 32 of each
+matrix(byrow=T, ncol=2, unlist(myTreeObj)[ grep("[0-9]$", names(unlist(myTreeObj)))])
+# when the children1 node is "", the children2 node is an output: or code: node.
+childrenNtable = ..()
+childrenNtable[childrenNtable[ , 1] == "", 2]  #Confirmed. 1 each for the 9 
