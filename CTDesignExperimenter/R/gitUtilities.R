428473ffaa61590c9192486f2f5bb71a2072677d
Roger Day <notsofeo@gmail.com>
2014-06-08 08:09:20 -0400

    makeItemFileName etc moved into gitUtilities.R

--- CTDesignExperimenter/R/gitUtilities.R
+++ CTDesignExperimenter/R/gitUtilities.R
@@ -12,35 +12,67 @@ writeSwapMeetFile = function(item,
   return(filename)
 }
 
+swapMeetDir = function(dir) {
+  if(!missing(dir)) {
+    assign('swapDir', dir, pos=1)
+    return(dir)
+  }
+  else if(exists(x = "swapDir", where = 1))
+    return(get("swapDir", pos=1))
+  else {
+    dir = "../CTDEswapmeet"  ## default
+    assign('swapDir', dir, pos=1)
+    return(dir)
+  }
+}
 
-writeVariableFile = function(variable, ...)
-  writeSwapMeetFile(variable, prefix=paste0("v_", variable@name), ...)
+makeItemFileName = function(item) {
+  if(!is(item, "SwapItem"))
+    stop("makeItemFileName: item should be a SwapItem")
+  if(item@description %in% "" | is.null(item@description)) 
+    stop("item@description cannot be empty")  
+  # NOTE: this sets the item's timestamp field.
+  item@timestamp = Sys.time()
+  #   and author field
+  item@author = Sys.getenv("USER")
+  prefix = switch(class(item),
+                  VariableGenerator="I_" %&% item@outputVariable@name,
+                  EligibilityCriterion="I_" %&% item@outputVariable@name,
+                  Variable="V_" %&% item@name,
+                  Scenario="S_" %&% item@name,
+                  ListOfInserts="L_" %&% "ListOfInserts", 
+                  Criterion="C_" %&% item@name,
+                  "UNKNOWN_item_type")
+  filename = paste0(prefix, "_",  
+                    "_", as.numeric(item@timestamp), ".R")
+  return(filename)
+}
 
-writeVariableGeneratorFile = function(variableGenerator)
-  writeSwapMeetFile(variableGenerator,
-       prefix=paste0("vg_", variableGenerator@outputVariable@name),
-      ...)
+makeItemFilePath = function(item, dir) {
+  return(swapMeetDir() %&% "/" %&% makeItemFileName(item))
+}
 
 writeInsertFile = function(insert, ...) {
   for(v in insert@requirements) {
-    if(!file.exists(paste0("../CTDEswapmeet/", v@filename))) {
-      write
+    if(!file.exists(paste0(swapMeetDir(), v@filename))) {
+      writeSwapMeetFile(v)
     }
   }
-    writeVariableFile(v, commitIt=TRUE)
   provlist = ifelse(is.list(insert@provisions), 
                     insert@provisions, list(insert@provisions))
-  for(v in insert@provisions)  
-    writeVariableFile(v, commitIt=TRUE)
-  writeSwapMeetFile(insert,
-        prefix=paste0("i_", insert@insertSubType, "_"), ...)
+  for(v in insert@provisions)  {
+    if(!file.exists(paste0(swapMeetDir(), v@filename))) {
+      writeVariableFile(v, commitIt=TRUE)
+    }
+    writeSwapMeetFile(insert)
+  }
 }
 
-writeScenarioFile = function(scenario, comment, ...) {
+writeScenarioFile = function(scenario,  ...) {
   if(missing(comment))
     comment = readline("Please write a comment for git (one line only): ")
   filenames = sapply(defaultScenario, writeInsertFile, commitIt=FALSE, ...)
-  commitSwapMeetFiles(filenames, comment=comment)
+  commitSwapMeetFiles(filenames, ...)
 }
 
 commitSwapMeetFiles = function(fileNames, dir="../CTDEswapmeet", comment) {
