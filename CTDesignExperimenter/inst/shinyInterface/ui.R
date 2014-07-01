affbb1af3ddf346aa39f213c682374639c14c075
Roger Day <notsofeo@gmail.com>
2014-07-01 00:51:39 -0400

    Improved conditionalPanels.

--- CTDesignExperimenter/inst/shinyInterface/ui.R
+++ CTDesignExperimenter/inst/shinyInterface/ui.R
@@ -26,45 +26,56 @@ conditionPanelNoneSelected = conditionalPanel(
       shinyalert(id = "cloneScen"),
       hr())
 )
-  
+
 outputPreamble <<- 'window.Shiny.shinyapp.$bindings.'
 
 leafDepthJSfunction = singleton(tags$script(
   "function leafDepth() { return " %&% outputPreamble %&% 
-    " treeSelectionDepth.el.textContent; }; "))  
+    " treeSelectionDepth.el.textContent; }; "))
+leafTextJSfunction = singleton(tags$script(
+  "function leafText() { return " %&% outputPreamble %&% 
+    " treeSelectionText.el.textContent; };
+   function is_needed() {return leafText().search('needs:') == 0;}; 
+   function is_code() {return leafText().search('generator code:') == 0;}; 
+   function is_param() {return leafText().search('param:') == 0;}; 
+   function is_provision() {return leafText().search('provides:') == 0;}; 
+  "))
 
 conditionPanel_1_insert = conditionalPanel(condition = 
-  'input.jstree1.length == 1 & (leafDepth() == 2)', 
-    #  '($("#jstree1").jstree().get_selected().length == 1)', # & (" %&%
-    #  outputPreamble %&% 'treeSelectionDepth == 2)',
-    ## THE FOLLOWING expression shows #j1_2 etc:
-    ## 'alert($("#jstree1").jstree().get_selected().toString())',  
-    actionButton(inputId="btnRemoveInsert" , label="Remove insert", styleclass = "success"),
-    actionButton(inputId="btnCloneInsert" , label="Clone insert", styleclass = "success"),
-    actionButton(inputId="btnEditInsert" , label="Edit insert", styleclass = "success"),
-    actionButton(inputId="btnAddRequirement" , label="Add a needed Variable", styleclass = "success"),
-    # NOT WORKING  textOutput("selectedNode"),
-    hr()
+       'input.jstree1.length == 1 & (leafDepth() == 2)', 
+     ## THE FOLLOWING expression shows #j1_2 etc:
+     ## 'alert($("#jstree1").jstree().get_selected().toString())',  
+     actionButton(inputId="btnRemoveInsert" , label="Remove insert", css.class = "treeclass_2"),
+     actionButton(inputId="btnCloneInsert" , label="Clone insert", css.class = "treeclass_2"),
+     actionButton(inputId="btnEditInsert" , label="Edit insert", css.class = "treeclass_2"),
+     actionButton(inputId="btnAddRequirement" , label="Add a needed Variable", css.class = "treeclass_2"),
+     hr()
 )
-# conditionPanel_1_variable = 
-#   conditionalPanel(condition = 
-#                      "($('#jstree1').jstree().get_selected().length == 1) & (" %&%
-#                      outputPreamble %&% 'treeSelectionDepth == 3)',
-#                    HTML("Selected one variable."),
-#                    textOutput("selectedNode"),
-#                    hr()
-#   )
-conditionPanelMoreThan1 = 
-  ###  Buttons when > 1 items are selected:
-  conditionalPanel(
-    '$("#jstree1").jstree().get_selected().length > 1',  #This works!! 0 1 2 etc.
-    # 'output.moreThanOneSelected == true', 
-                   actionButton(inputId="btnSaveListOfInserts" , label="btnSaveListOfInserts", styleclass = "success"),
-                   textOutput("selectedNodes"),
+conditionPanel_1_vg_code = 
+  conditionalPanel(condition = 
+                     'input.jstree1.length == 1 & (leafDepth() == 3)
+                   & is_code()', 
+                   actionButton(inputId="btnEditCode" , label="Edit code", css.class = "treeclass_3"),
                    hr()
   )
-
-
+conditionPanel_1_needed_var = 
+  conditionalPanel(condition = 
+                     'input.jstree1.length == 1 & (leafDepth() == 3)
+                   & is_needed()', 
+                   HTML("Selected one variable."),
+                   actionButton(inputId="btnRemoveVariable" , label="Remove Variable", css.class = "treeclass_3"),
+                   actionButton(inputId="btnCloneVariable" , label="Clone Variable", css.class = "treeclass_3"),
+                   actionButton(inputId="btnEditVariable" , label="Edit Variable", css.class = "treeclass_3"),
+                   hr()
+  )
+conditionPanel_moreThan1_insert = 
+  conditionalPanel(
+    '$("#jstree1").jstree().get_selected().length > 1
+    & (leafDepth() == 2)',
+    actionButton(inputId="btnSaveListOfInserts" , label="btnSaveListOfInserts", css.class = "treeclass_2"),
+    textOutput("selectedNodes"),
+    hr()
+  )
 
 scenarioPanel = tabPanel("Current scenario",
                          #div(class="row-fluid span1",
@@ -73,8 +84,9 @@ scenarioPanel = tabPanel("Current scenario",
                                    value=currentScenario@name),
                          conditionPanelNoneSelected,
                          conditionPanel_1_insert,
-#                         conditionPanel_1_variable,
-                         conditionPanelMoreThan1,
+                         conditionPanel_1_vg_code,
+                         conditionPanel_1_needed_var,
+                         conditionPanel_moreThan1_insert,
                          conditionalPanel(condition = 
                                             '$("#jstree1").jstree().get_selected().length > 0',
                                           div(class="row-fluid",
@@ -90,20 +102,19 @@ scenarioPanel = tabPanel("Current scenario",
                                             ) 
                          ),
                          div(style="overflow:auto; height:800px", 
-                         myTree),
+                             myTree),
                          tagToOpenTree 
 )
 
 variableEditorPanel = #conditionalPanel(condition = 'true',
   # Sadly, cannot put a conditionalPanel in a tabsetPanel.
-                        tabPanel("Edit var",
-                          HTML("This is the variableEditor panel."),
-                          actionButton(inputId="btnCreateVar" , 
-                                       label="Create new Variable", styleclass = "success")
-                          #,conditionalPanel(condition = 'input.btnCreateVar')
-                          
-                        )
-                                       #)
+  tabPanel("Edit var",
+           HTML("This is the variableEditor panel."),
+           actionButton(inputId="btnCreateVar" , 
+                        label="Create new Variable", css.class = "success")
+           #,conditionalPanel(condition = 'input.btnCreateVar')
+  )
+#)
 
 CSSreference = singleton(tags$head(tags$link(href = "ctde.css", 
                                              rel = "stylesheet")))
@@ -114,10 +125,11 @@ CSSreference = singleton(tags$head(tags$link(href = "ctde.css",
 
 
 myJSincludes = tagList(
-  CSSreference ### OK. Works (for text colors)
-  , includeScript("www/ctde-types.js") ## It does find this !
+  includeScript("www/ctde-types.js") ## It does find this !
   , includeScript("www/ss-jstree.js")  # and this.
+  , CSSreference ### OK. Works (for text colors)
   , leafDepthJSfunction
+  , leafTextJSfunction
 )
 ## The context menu appears with the standard menu, not in place of.
 #  scriptToGetDepths?
@@ -131,10 +143,7 @@ shinyUI(
     header=tagList(myJSincludes,
                    hr(),
                    uiOutput(outputId="debugTools"),
-                   hr(),
-                   conditionalPanel(condition = 'true',
-                                    textOutput('treeSelectionDepth' 
-                                              ))),
+                   hr()),
     # message-handler code causes hang.
     #       singleton(
     #         tags$head(tags$script(src = "message-handler.js"))
@@ -145,7 +154,7 @@ shinyUI(
                tabPanel("Create new Variable"),
                tabPanel("Create new Insert")
     ),
-#    insertEditorPanel,
+    #    insertEditorPanel,
     tabPanel("One CT run", 
              "Display results from a single CT run for the selected scenario.",
              hr(),
