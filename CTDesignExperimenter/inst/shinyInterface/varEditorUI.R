3a5e2ed49b5bce8fdcf527ba50e87e158d1c1a07
Roger Day <notsofeo@gmail.com>
2014-07-26 11:49:25 -0400

    Converted Var search to datatables. We can pick up the click event and get info.

--- CTDesignExperimenter/inst/shinyInterface/varEditorUI.R
+++ CTDesignExperimenter/inst/shinyInterface/varEditorUI.R
@@ -1,20 +1,67 @@
-output$varEditorUI = renderUI({ 
-  theVar = rValues$theVar
-  catn("output$varEditorUI: var is ", capture.output(theVar))
-  CHECK<-capture.output(theVar@checkDataType)
-  CHECK = gsub("[\t ]+", " ", 
-       paste(collapse="", 
-             CHECK [-length(CHECK)] ) ) 
-  catn("CHECK: ", CHECK)
-  result = data.frame(
+varToDataframe = function(theVar){
+  data.frame(
     name=theVar@name,
     description=theVar@description,
-    checkDataType = CHECK ,
+    checkDataType = printFunctionBody(theVar@checkDataType),
     author=theVar@author,
     timestamp=capture.output(theVar@timestamp),
     filename=theVar@filename      
   )
-  print(result)
+}
+output$varEditorUI = renderUI({ 
+  theVar = rValues$theVar
+  CHECK = printFunctionBody(theVar@checkDataType)
+  catn("output$varEditorUI: var is ", capture.output(theVar))
+  vFilenames <<- rev(dir(swapMeetDir(), pattern = "^V_"))
+  allVariablesList = lapply(vFilenames, function(fname) {
+    theVar = source(swapMeetDir() %&% fname, local=TRUE)$value
+    varToDataframe(theVar)
+  })
+  allVariablesDF <<- Reduce(rbind, allVariablesList)
+  allVarnames <<- data.frame(name=unique(allVariablesDF$name))
+  output$allVariablesTable <<- renderDataTable(allVariablesDF,
+               options=list(
+                 fnInitComplete = I("function(oSettings, json) {
+                                    //alert('Done.');
+                                    console.log('Done.');
+                                    }")
+                 # fnInitComplete works.
+                 , fnRowCallback= I(
+               " function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
+                    console.log('fnRowCallback', aData, iDisplayIndex, iDisplayIndexFull);
+                    // this
+                  }")
+               )
+               , callback="function(oTable) {
+                    alert('callback');
+                    oTable.on('click', function() {
+                      alert('Row clicked.');
+                     console.log('Row Clicked.' , this);
+                    // this.parent().find('td').each(function() {
+                    //    console.log(this.html());
+                    //  });
+                   });
+               }"
+#                    
+#                    // Cell click
+#                    $('td', nRow).on('click', function() {
+#                      console.log('Col Clicked.', this, aData, iDisplayIndex, iDisplayIndexFull);
+#                    });
+   )
+  
+#                ,
+#                callback = 'function(oTable) {
+#                   /*"initComplete": function () {*/
+#                       var api = this.api();
+#                       api.$("td").click( function () {
+#                           
+#                           api.search( this.innerHTML ).draw();
+#                           input.selectedTableRow = api.search( this.innerHTML );
+#                       } );
+#                     }
+#                   } 
+#                 ' )
+  ### RETURN UI
   div(
     HTML(" Editing selected Variable"), 
     hr(),
@@ -27,22 +74,63 @@ output$varEditorUI = renderUI({
     actionButton(inputId="btnSaveVarAs" , 
                  label="Save variable as...", css.class = "treeClass-3"),
     hr(),
-    conditionalPanel("input.btnSearchVar > 0",
-                     select2Input("varSearchFileInput", "varSearchFileInput",
-                                 #selectize=FALSE,
-                                 choices=rev(dir(swapMeetDir(), pattern = "^V_")),
-                                 width="100%")),
+    conditionalPanel("input.btnSearchVar > 0", 
+                     "HERE is allVariablesTable",
+                     dataTableOutput("allVariablesTable"))
+    ,
+  #                      textInput.typeahead(id="searchTypeAhead", "Search typeahead var name",
+  #                                          local=allVarnames,
+  #                                          tokens=1:nrow(allVarnames), #gsub("V_", "", vFilenames),
+  #                                          valueKey="name", 
+  #                                          template=HTML('<p>{{name}}</p>')
+  #                      )
+  #   textInput.typeahead(id="searchTypeAhead", "LABEL", 
+  #                     local=allVariablesDF, 
+  #                     tokens=paste(allVariablesDF$name,
+  #                                  allVariablesDF$description), 
+  #                     valueKey="filename", 
+  #                     template="<p style='width:2000px'>{{name}} : {{description}}")
+  #       textInput.typeahead(id="searchTypeAhead", "Search typeahead",
+  #                           local=allVariablesDF,
+  #                           tokens=1:nrow(allVariablesDF), #gsub("V_", "", vFilenames),
+  #                           valueKey="filename", 
+  #                           template=
+  #                             #tagAppendAttributes(
+  #                               '<p style="width:100%">{{filename}} :  {{description}}</p>' #) 
+          # select2Input WORKS, and variable loads, but not what we want.
+#           select2Input("varSearchFileInput", "varSearchFileInput",
+#                      #selectize=FALSE,
+#                      choices=vFilenames,
+#                      style="width:100%"
+#                      )
     #fileInput("varSearchFileInput", "varSearchFileInput"))
     hr(),
     textInput("varName", label = "name", theVar@name),
-    textInput("varDescription", label = "description", theVar@description),
-    textInput("varCHECK", label = "check", CHECK),
+    tagAppendAttributes(div(
+              textInput("varDescription", label = "description", theVar@description)),
+              style="width:100%"),
+    textInput("varCHECK", label = "check", printFunctionBody(theVar@checkDataType)),
     hr(),
     renderText({"author: " %&% theVar@author}),
     renderText({"timestamp: " %&% capture.output(theVar@timestamp)}),
     renderText({"file: " %&% theVar@filename})
   )
 })
+
+observe(label="searchVariableObserver", {
+  catn("searchVariableObserver: input$btnSearchVar = ", 
+       input$btnSearchVar)
+  if(!is.null(input$btnSearchVar))
+    if(input$btnSearchVar > 0) 
+      if(!is.null(input$searchTypeAhead)) 
+        if(input$searchTypeAhead != "") {
+          theVar = try(source(swapMeetDir() %&% input$searchTypeAhead, 
+                              local = TRUE)$value) 
+          if(class(theVar) == "Variable") rValues$theVar = theVar
+          else shinyalert("Sorry, it wasn't a Variable file.")
+        }
+})
+
 
 observe({       ### Find and load a variable from a file.
   if(input$tabsetID=="Editors" & !is.null(input$btnSearchVar)){
