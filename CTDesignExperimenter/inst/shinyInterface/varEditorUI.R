9b13e8b9e124b88b2109695577f8b99a248cc6c2
Roger Day <notsofeo@gmail.com>
2014-08-02 21:36:38 -0400

    radioButtons to select and load variable-- works!

--- CTDesignExperimenter/inst/shinyInterface/varEditorUI.R
+++ CTDesignExperimenter/inst/shinyInterface/varEditorUI.R
@@ -18,110 +18,124 @@ output$varEditorUI = renderUI({
     varToDataframe(theVar)
   })
   allVariablesDF <<- Reduce(rbind, allVariablesList)
+  radioButtons = sapply(1:nrow(allVariablesDF),
+                        function(rownum)
+                          HTML("<input type=\"radio\" name=\"chooseVariable\" 
+                id=\"chooseVariable" %&% rownum
+                               %&% "\" value=" %&% rownum %&% ">"))
+  allVariablesDF = data.frame(select=radioButtons, allVariablesDF) 
+  
   allVarnames <<- data.frame(name=unique(allVariablesDF$name))
   output$allVariablesTable <<- renderDataTable(allVariablesDF,
-               options=list(
-                 fnInitComplete = I("function(oSettings, json) {
+        options=list(
+            fnInitComplete = I("function(oSettings, json) {
                                     //alert('Done.');
                                     console.log('Done.');
                                     }")
-                 # fnInitComplete works.
-                 , fnRowCallback= I(
-               " function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
+           # fnInitComplete works.
+          , fnRowCallback= I(
+              " function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
                     $(nRow).on('click', function() {
                       console.log('Row Clicked. ', 
-                        this, aData, iDisplayIndex, iDisplayIndexFull);
-                      });
-                    //console.log('fnRowCallback', aData, iDisplayIndex, iDisplayIndexFull);
-                    // this
+                        this, aData, aData[5],
+                        iDisplayIndex, iDisplayIndexFull);
+                      $(nRow).bgColor = '#131';
+                      window.Shiny.shinyapp.$values['fileToLoad']
+                         = aData[5];
+                      //nRow.addClass('rowClicked');
+                    });
+                    window.DollarnRow = $(nRow);
+                    window.nRow = nRow;
+                    window.aData = aData;
                   }")
-               )
-               , callback="function(oTable) {
+              )
+              , callback="function(oTable) {
                     // This approach does not get me the cell info.
                     oTable.on('click', function(el) {
                       window.thisRow = el;
 //thisRow.currentTarget is HTMLTableElement
 //thisRow.currentTarget.children[0].innerHTML It's the header row!
                       //alert('Row clicked.' + this); // HTMLTableElement
-                     console.log('Row clicked. ', this.getLastChild());
-                      this.setBgColor('blue');
                       //this.parent().find('td').each(function() {
                       //  console.log(this.html());
                       //});
                    });
                }"
-#                    
-#                    // Cell click
-#                    $('td', nRow).on('click', function() {
-#                      console.log('Col Clicked.', this, aData, iDisplayIndex, iDisplayIndexFull);
-#                    });
-   )
+                                               #                    
+                                               #                    // Cell click
+                                               #                    $('td', nRow).on('click', function() {
+                                               #                      console.log('Col Clicked.', this, aData, iDisplayIndex, iDisplayIndexFull);
+                                               #                    });
+  )
   
-#                ,
-#                callback = 'function(oTable) {
-#                   /*"initComplete": function () {*/
-#                       var api = this.api();
-#                       api.$("td").click( function () {
-#                           
-#                           api.search( this.innerHTML ).draw();
-#                           input.selectedTableRow = api.search( this.innerHTML );
-#                       } );
-#                     }
-#                   } 
-#                 ' )
+  #                ,
+  #                callback = 'function(oTable) {
+  #                   /*"initComplete": function () {*/
+  #                       var api = this.api();
+  #                       api.$("td").click( function () {
+  #                           
+  #                           api.search( this.innerHTML ).draw();
+  #                           input.selectedTableRow = api.search( this.innerHTML );
+  #                       } );
+  #                     }
+  #                   } 
+  #                 ' )
   ### RETURN UI
   div(
-    HTML(" Editing selected Variable"), 
+    HTML(" Variable Editor "),
     hr(),
-    actionButton(inputId="btnNewVar" , 
-                 label="New variable", css.class = "treeClass-3"),
-    actionButton(inputId="btnSearchVar" , 
-                 label="Find and load variable", css.class = "treeClass-3"),
-    actionButton(inputId="btnSaveVar" , 
-                 label="Save variable in scenario", css.class = "treeClass-3"),
-    actionButton(inputId="btnSaveVarAs" , 
-                 label="Save variable as...", css.class = "treeClass-3"),
-    hr(),
-    conditionalPanel("input.btnSearchVar > 0", 
-                     "HERE is allVariablesTable",
-                     dataTableOutput("allVariablesTable"))
-    ,
-  #                      textInput.typeahead(id="searchTypeAhead", "Search typeahead var name",
-  #                                          local=allVarnames,
-  #                                          tokens=1:nrow(allVarnames), #gsub("V_", "", vFilenames),
-  #                                          valueKey="name", 
-  #                                          template=HTML('<p>{{name}}</p>')
-  #                      )
-  #   textInput.typeahead(id="searchTypeAhead", "LABEL", 
-  #                     local=allVariablesDF, 
-  #                     tokens=paste(allVariablesDF$name,
-  #                                  allVariablesDF$description), 
-  #                     valueKey="filename", 
-  #                     template="<p style='width:2000px'>{{name}} : {{description}}")
-  #       textInput.typeahead(id="searchTypeAhead", "Search typeahead",
-  #                           local=allVariablesDF,
-  #                           tokens=1:nrow(allVariablesDF), #gsub("V_", "", vFilenames),
-  #                           valueKey="filename", 
-  #                           template=
-  #                             #tagAppendAttributes(
-  #                               '<p style="width:100%">{{filename}} :  {{description}}</p>' #) 
-          # select2Input WORKS, and variable loads, but not what we want.
-#           select2Input("varSearchFileInput", "varSearchFileInput",
-#                      #selectize=FALSE,
-#                      choices=vFilenames,
-#                      style="width:100%"
-#                      )
-    #fileInput("varSearchFileInput", "varSearchFileInput"))
-    hr(),
-    textInput("varName", label = "name", theVar@name),
-    tagAppendAttributes(div(
-              textInput("varDescription", label = "description", theVar@description)),
-              style="width:100%"),
-    textInput("varCHECK", label = "check", printFunctionBody(theVar@checkDataType)),
-    hr(),
-    renderText({"author: " %&% theVar@author}),
-    renderText({"timestamp: " %&% capture.output(theVar@timestamp)}),
-    renderText({"file: " %&% theVar@filename})
+    div(class='col-6',
+        actionButton(inputId="btnNewVar" , 
+                     label="New variable", css.class = "treeClass-3"),
+        actionButton(inputId="btnSearchVar" , 
+                     label="Find and load variable", css.class = "treeClass-3"),
+        actionButton(inputId="btnSaveVar" , 
+                     label="Save variable in scenario", css.class = "treeClass-3"),
+        actionButton(inputId="btnSaveVarAs" , 
+                     label="Save variable as...", css.class = "treeClass-3"),
+        hr(),
+        #                      textInput.typeahead(id="searchTypeAhead", "Search typeahead var name",
+        #                                          local=allVarnames,
+        #                                          tokens=1:nrow(allVarnames), #gsub("V_", "", vFilenames),
+        #                                          valueKey="name", 
+        #                                          template=HTML('<p>{{name}}</p>')
+        #                      )
+        #   textInput.typeahead(id="searchTypeAhead", "LABEL", 
+        #                     local=allVariablesDF, 
+        #                     tokens=paste(allVariablesDF$name,
+        #                                  allVariablesDF$description), 
+        #                     valueKey="filename", 
+        #                     template="<p style='width:2000px'>{{name}} : {{description}}")
+        #       textInput.typeahead(id="searchTypeAhead", "Search typeahead",
+        #                           local=allVariablesDF,
+        #                           tokens=1:nrow(allVariablesDF), #gsub("V_", "", vFilenames),
+        #                           valueKey="filename", 
+        #                           template=
+        #                             #tagAppendAttributes(
+        #                               '<p style="width:100%">{{filename}} :  {{description}}</p>' #) 
+        # select2Input WORKS, and variable loads, but not what we want.
+        #           select2Input("varSearchFileInput", "varSearchFileInput",
+        #                      #selectize=FALSE,
+        #                      choices=vFilenames,
+        #                      style="width:100%"
+        #                      )
+        #fileInput("varSearchFileInput", "varSearchFileInput"))
+        hr(),
+        textInput("varName", label = "name", theVar@name),
+        tagAppendAttributes(div(
+          textInput("varDescription", label = "description", theVar@description)),
+          style="width:100%"),
+        textInput("varCHECK", label = "check", printFunctionBody(theVar@checkDataType)),
+        hr(),
+        renderText({"author: " %&% theVar@author}),
+        renderText({"timestamp: " %&% capture.output(theVar@timestamp)}),
+        renderText({"file: " %&% theVar@filename})
+    ),
+    div(class='col-6',
+        conditionalPanel("input.btnSearchVar > 0", 
+                         hr(),
+                         dataTableOutput("allVariablesTable"))
+    )
   )
 })
 
@@ -144,9 +158,9 @@ observe({       ### Find and load a variable from a file.
   if(input$tabsetID=="Editors" & !is.null(input$btnSearchVar)){
     if(input$btnSearchVar > 0 & !is.null(input$varSearchFileInput)) {
       try({
-#         fileInfo = input$varSearchFileInput
-#         print(fileInfo)
-#        theVar = source(fileInfo$datapath, local = TRUE)$value
+        #         fileInfo = input$varSearchFileInput
+        #         print(fileInfo)
+        #        theVar = source(fileInfo$datapath, local = TRUE)$value
         theVar = source(swapMeetDir() %&% input$varSearchFileInput, 
                         local = TRUE)$value
         print(str(theVar))
@@ -172,8 +186,8 @@ observe({       ### Save Variable in a swapMeet file.
     if(input$btnSaveVarAs > 0) {
       theVar = try(
         Variable(name = input$varName, 
-                        description = input$varDescription, 
-                        checkDataType = eval(parse(text=input$varCHECK)))) 
+                 description = input$varDescription, 
+                 checkDataType = eval(parse(text=input$varCHECK)))) 
       if(class(theVar) == "try-error")
         shinyalert("Error in variable: " %&% theVar)
       else {
@@ -182,4 +196,14 @@ observe({       ### Save Variable in a swapMeet file.
       }
     }
   }
+})
+
+observe({
+  varFileName = allVariablesDF[input$chooseVariable, "filename"]
+  catn("varFileName = ", varFileName)
+  theVar = try(
+    source(swapMeetDir() %&% varFileName, local=TRUE)$value
+  )
+  if(class(theVar) != "try-error")
+    rValues$theVar = theVar
 })
