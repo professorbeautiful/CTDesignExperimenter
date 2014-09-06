fd06d140f26628d40b5d233232a4eb6306f86b22
Roger Day <notsofeo@gmail.com>
2014-09-06 12:48:55 -0400

    Better keep reloadScenario around for now.

--- CTDesignExperimenter/inst/shinyInterface/global.R
+++ CTDesignExperimenter/inst/shinyInterface/global.R
@@ -6,7 +6,17 @@ scaffoldObjectNames = scaffoldObjects[[1]]
 
 source("makeTree.R")
 
-
+reloadScenario = function() {  
+  scenarioTree <<- makeTree(scenario=currentScenario, "full")
+  # length(scenarioTree) is 13
+  myTreeObj <<- myjstree.obj(scenarioTree)  # shiny.tag
+  # class attributes are added in myjstree.obj
+  # length(myTreeObj[[3]][[1]]) is 13
+  myTree <<- jstree("jstreeScenario", myTreeObj)  # shiny.tag.list
+}
+currentScenario = defaultScenario  
+#reloadScenario()
+  
 experimentTable = data.frame(sampleSize=NA)
 
 
