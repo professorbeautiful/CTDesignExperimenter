f36328a102a2f4ccad7cc87452b1fbd806c21699
Roger Day <notsofeo@gmail.com>
2014-11-02 14:07:03 -0500

    Working on icons in ctde-types.js.

--- CTDesignExperimenter/inst/shinyInterface/www/ctde-types.js
+++ CTDesignExperimenter/inst/shinyInterface/www/ctde-types.js
@@ -1,25 +1,25 @@
 $(function () {
   $("#scenarioTree").jstree({
     "types" : {
-    "types" : {
-      "level_1" : {
-            'icon' : { 'image' :  url('BLOCK32.png') }
-      },
-      "level_2" : {
-            'icon' : { 'image' :  url('Insert.png') }
-      }
-    },
+	    "types" : {
+	      "level_1" : {
+		    'icon' : { 'image' :  'BLOCK32.png' }
+	      },
+	      "level_2" : {
+		    'icon' : { 'image' :  '/Insert.png' }
+	      }
+	    },
     },
     // "plugins" : [ "types", "contextmenu", "wholerow" ]
     "plugins" : [ "contextmenu", "types"],
-	"contextmenu": {
+    "contextmenu": {
 	    "items": {
 		"create" : false,
 		"ccp" : false,
 		"rename" : false,
 		"remove" : {
 		    "label" : "&nbsp;Delete",
-		    "icon" : "/images/icon/cross.png"
+		    "icon" : "/Var32.png"
 		}
 	    }
 	}
