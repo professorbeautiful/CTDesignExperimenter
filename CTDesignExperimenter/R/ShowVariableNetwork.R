171e3eb11fdd8102ccd32951d76f05a45076975a
Roger Day <notsofeo@gmail.com>
2014-12-11 11:48:28 -0500

    Working towards a VN status report.

--- CTDesignExperimenter/R/ShowVariableNetwork.R
+++ CTDesignExperimenter/R/ShowVariableNetwork.R
@@ -6,21 +6,10 @@ vgList = VariableGeneratorList(getVGs(currentScenario@inserts,
                                       "DesignParameter"))
 designVN = VariableNetwork(vgList=vgList)
 
-designVN@vgList
-designVN@  allProvisions="list",
-designVN@           allProvisionNames
-designVN@           provisionMap
-designVN@           allRequirements
-designVN@           allRequirementNames
-designVN@           requirementMap
-designVN@           requirementMatrix="ANY",
-designVN@           howManyNeedMe
-designVN@           candidates
-
-
 candidateCounts = apply(designVN@candidates, 1, sum)
 
 clashingVariables = names(candidateCounts)[candidateCounts > 1]
+satisfiedVariables = names(candidateCounts)[candidateCounts == 1]
 
 unsatisfiedVariables = designVN@requirements  ### requirements not satisfied internally
 #check
