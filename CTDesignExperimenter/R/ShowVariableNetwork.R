c94642f6cc70149d85a0eeb7ea48e676607c3b51
Roger Day <notsofeo@gmail.com>
2014-12-16 14:08:27 -0500

    Work in progress...

--- CTDesignExperimenter/R/ShowVariableNetwork.R
+++ CTDesignExperimenter/R/ShowVariableNetwork.R
@@ -1,17 +1,17 @@
-### ShowVariableNetworks
-
-
-
-vgList = VariableGeneratorList(getVGs(currentScenario@inserts, 
-                                      "DesignParameter"))
-designVN = VariableNetwork(vgList=vgList)
-
-candidateCounts = apply(designVN@candidates, 1, sum)
-
-clashingVariables = names(candidateCounts)[candidateCounts > 1]
-satisfiedVariables = names(candidateCounts)[candidateCounts == 1]
-
-unsatisfiedVariables = designVN@requirements  ### requirements not satisfied internally
-#check
-identical(names(unsatisfiedVariables), names(candidateCounts)[candidateCounts == 0]
-
+#### ShowVariableNetworks
+#### In progress...
+#
+#
+#vgList = VariableGeneratorList(getVGs(currentScenario@inserts, 
+#                                      "DesignParameter"))
+#designVN = VariableNetwork(vgList=vgList)
+#
+#candidateCounts = apply(designVN@candidates, 1, sum)
+#
+#clashingVariables = names(candidateCounts)[candidateCounts > 1]
+#satisfiedVariables = names(candidateCounts)[candidateCounts == 1]
+#
+#unsatisfiedVariables = designVN@requirements  ### requirements not satisfied internally
+##check
+#identical(names(unsatisfiedVariables), names(candidateCounts)[candidateCounts == 0]
+#
