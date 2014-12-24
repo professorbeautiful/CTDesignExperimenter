244305227a81344ad30a613204c105501a3e7bae
Roger Day <notsofeo@gmail.com>
2014-12-23 23:54:24 -0500

    This new file handles the searching for scenarios.

---
+++ CTDesignExperimenter/inst/shinyInterface/scenarioSearchTableUI.R
@@ -0,0 +1,35 @@
+output$scenarioSearchTable = renderUI({
+  
+  pattern = "^S_"
+  objectTypeName="Scenario"
+  source("createSwapMeetObjectTable.R", local=TRUE)  
+  
+  observeChooseScenario = observe(label="observeChooseScenario", {
+    chooseScenario = input$chooseScenario ## reactivity here 
+    catn("observerChooseScenario:  chooseScenario number= ", chooseScenario)
+    if(!is.null(chooseScenario))
+      isolate({
+        scenarioFileName = allScenariosDF[chooseScenario, "filename"]
+        catn("observerChooseScenario:  FileName = ", scenarioFileName)
+        currentScenario = try(
+          source(swapMeetDir() %&% scenarioFileName, local=TRUE)$value
+        )
+        if(class(currentScenario) != "try-error")
+          rValues$currentScenario <- currentScenario
+      })
+  })
+  
+  div(class='col-6',
+      h3("SEARCHING SCENARIOS"),
+      conditionalPanel(
+        "input.btnSearchScenario > 0", 
+        hr(),
+        tagAppendAttributes(a(""), id="idSearchScenario"),
+        h3("Click on the radiobutton to load the Scenario into the template above."),
+        HTML('<div id="chooseScenario" class="control-group shiny-input-radiogroup">
+                   <label class="control-label" for="chooseScenario">Swapmeet Scenarios</label>'),
+        dataTableOutput("allScenariosTable"),
+        HTML('</div>')
+      )
+  )
+})
