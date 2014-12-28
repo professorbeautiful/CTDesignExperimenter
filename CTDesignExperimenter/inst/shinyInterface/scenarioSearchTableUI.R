1efe83f104f7b677f68d7130d68e505cb6ed0946
Roger Day <notsofeo@gmail.com>
2014-12-27 20:29:53 -0500

    Move the SEARCHING SCENARIOS into conditionalPanel.

--- CTDesignExperimenter/inst/shinyInterface/scenarioSearchTableUI.R
+++ CTDesignExperimenter/inst/shinyInterface/scenarioSearchTableUI.R
@@ -20,10 +20,10 @@ output$scenarioSearchTable = renderUI({
   })
   
   div(class='col-6',
-      h3("SEARCHING SCENARIOS"),
       conditionalPanel(
         "input.btnSearchScenario > 0", 
         hr(),
+        h3("SEARCHING SCENARIOS"),
         tagAppendAttributes(a(""), id="idSearchScenario"),
         h3("Click on the radiobutton to load the Scenario into the template above."),
         HTML('<div id="chooseScenario" class="control-group shiny-input-radiogroup">
