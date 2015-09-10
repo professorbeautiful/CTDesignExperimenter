a3803fa7ab87cc7e65ec2c85cfd88ed40dfcd4e1
Roger Day <notsofeo@gmail.com>
2015-09-10 13:13:58 -0400

    CTDesignExplorerDevel is deprecated.

--- CTDesignExplorerDevel/R/zzz.R
+++
@@ -1,6 +0,0 @@
-
-.onAttach = function(libname, pkgname) {
-	desc <- packageDescription(pkgname)
-	packageStartupMessage("This is ", pkgname, " ", desc$Version, " ", desc$Date)
-	return(invisible(NULL))
-}
